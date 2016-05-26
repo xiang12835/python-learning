c242ec087dc51a92871f535c52b1779c891a879a
yuxiang <yuxiang@youku.com>
2016-05-26 13:30:16 +0800

    modify .gitignore

---
+++ property/property.py
@@ -0,0 +1,13 @@
+class Student(object):
+
+    @property
+    def birth(self):
+        return self._birth
+
+    @birth.setter
+    def birth(self, value):
+        self._birth = value
+
+    @property
+    def age(self):
+        return 2016 - self._birth
