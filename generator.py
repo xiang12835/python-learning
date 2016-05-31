ef97785538619bd356bbe3737a94b80c1ff6e63d
yuxiang <yuxiang@youku.com>
2016-05-31 14:53:21 +0800

    add triangles

--- generator.py
+++ generator.py
@@ -41,3 +41,20 @@ f = g_fib(6)
 for g in f:
     print g
 
+
+'''练习: 杨辉三角'''
+def triangles():
+    t = [1]
+    while True:
+        yield t
+        t.append(0)
+        t = [t[i-1] + t[i] for i in range(len(t))]
+
+n = 0
+for t in triangles():
+    print t
+    n = n + 1
+    if n == 10:
+        break
+
+
