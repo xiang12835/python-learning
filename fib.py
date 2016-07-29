d93889891c2d92b65c7a7385acefa91d3696578d
yuxiang <yuxiang@youku.com>
2016-07-29 14:23:36 +0800

    fib

---
+++ fib.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+
+# 用函数实现一个斐波拉契数列
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        print(b)
+        a, b = b, a + b
+        n = n + 1
+    return 'done'
+
+fib(10)
+
+# 上面的函数和generator仅一步之遥。要把fib函数变成generator，只需要把print(b)改为yield b就可以了
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        yield b
+        a, b = b, a + b
+        n = n + 1
+
+f = fib(6)
+print f
