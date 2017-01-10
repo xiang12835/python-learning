a852d7fd9a1d3bf9c98b982e0acc55bdb0816846
yuxiang <yu.xiang@alibaba-inc.com>
2017-01-10 21:09:46 +0800

    add urllib2

---
+++ module/urllib2.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+
+import urllib
+import urllib2
+
+url = 'http://www.pythontab.com'
+user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+
+values = {'name': 'Michael Foord',
+          'location': 'pythontab',
+          'language': 'Python'}
+headers = {'User-Agent': user_agent}  # 添加headers到你的HTTP请求，模拟成Internet Explorer
+data = urllib.urlencode(values)  # 有时候你希望发送一些数据到URL(通常URL与CGI[通用网关接口]脚本，或其他WEB应用程序挂接)
+req = urllib2.Request(url, data, headers)
+response = urllib2.urlopen(req)
+the_page = response.read()
