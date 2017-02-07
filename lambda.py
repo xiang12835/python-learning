700e7a5337ed4fa3f6ffaff89add847efd7590f2
yuxiang <yu.xiang@alibaba-inc.com>
2017-02-07 14:53:35 +0800

    lambda

--- lambda.py
+++ lambda.py
@@ -19,3 +19,26 @@ def build(x, y):
     return lambda :x*x + y*y
 
 print build(3, 4)
+
+
+# 面试题
+def multipliers():
+    return [lambda x: i*x for i in range(4)]
+
+print [m(2) for m in multipliers()]
+# [6, 6, 6, 6]
+
+'''
+上述问题产生的原因是Python闭包的延迟绑定。这意味着内部函数被调用时，参数的值在闭包内进行查找。
+因此，当任何由multipliers()返回的函数被调用时，i的值将在附近的范围进行查找。
+那时，不管返回的函数是否被调用，for循环已经完成，i被赋予了最终的值3。
+'''
+
+# 解决
+# 一种解决方法就是用Python生成器。
+
+def multipliers():
+  for i in range(4): yield lambda x : i * x
+
+print [m(2) for m in multipliers()]
+
