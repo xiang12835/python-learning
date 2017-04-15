e4d4a49d8b3be7f62e0933744686c7f9c921b2c8
yu.xiang <yu.xiang@alibaba-inc.com>
2017-04-16 00:47:40 +0800

    add bubble_sort

---
+++ algorithm/bubble_sort.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+
+
+# 冒泡排序
+def bubble_sort(lists):
+    """ 
+    它重复地走访过要排序的数列，一次比较两个元素，如果他们的顺序错误就把他们交换过来。
+    走访数列的工作是重复地进行直到没有再需要交换，也就是说该数列已经排序完成。
+    """
+    count = len(lists)
+    for i in range(0, count):
+        for j in range(i + 1, count):
+            if lists[i] > lists[j]:
+                lists[i], lists[j] = lists[j], lists[i]
+    return lists
