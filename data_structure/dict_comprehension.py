1612834be7f81f9d3490407048570257b3aa5387
yuxiang <yuxiang0128@gmail.com>
2017-05-18 23:24:37 +0800

    add_dict_comprehension

---
+++ data_structure/dict_comprehension.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+
+
+# 用字典推导式构建字典序列
+l = [("a", 1), ["b", 2]]
+d = {key: value for (key, value) in l}
+print d
+
+
+# 用zip()构建字典序列
+a = [1, 2, 3]
+b = ['a', 'b', 'c']
+lst = zip(a, b)
+print lst
+
+dic = dict(lst)
+print dic
