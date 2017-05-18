2bb73cc229e36249ee75547bc41028bff49a5e34
yuxiang <yuxiang0128@gmail.com>
2017-05-18 17:33:50 +0800

    add self_and_cls_and_instance_method

---
+++ Interview/self_and_cls_and_instance_method.py
@@ -0,0 +1,41 @@
+# coding=utf-8
+
+
+def foo(x):
+    print "executing foo(%s)" % x
+
+
+class A(object):
+    def foo(self, x):
+        print "executing foo(%s, %s)" % (self, x)
+
+    @classmethod
+    def class_foo(cls, x):
+        print "executing class_foo(%s, %s)" % (cls, x)
+
+    @staticmethod
+    def static_foo(x):
+        print "executing static_foo(%s)" % x
+
+foo("normal method!")
+
+a = A()
+
+a.foo("instance method!")
+
+a.class_foo("class method!")
+A.class_foo("class method!")
+
+a.static_foo("static method!")
+A.static_foo("static method!")
+
+
+"""
+这里先理解下函数参数里面的self和cls.这个self和cls是对类或者实例的绑定,对于一般的函数来说我们可以这么调用foo(x),这个函数就是最常用的,它的工作跟任何东西(类,实例)无关.
+
+对于实例方法,我们知道在类里每次定义方法的时候都需要绑定这个实例,就是foo(self, x),为什么要这么做呢?因为实例方法的调用离不开实例,我们需要把实例自己传给函数,调用的时候是这样的a.foo(x)(其实是foo(a, x)).
+
+类方法一样,只不过它传递的是类而不是实例,A.class_foo(x).注意这里的self和cls可以替换别的参数,但是python的约定是这俩,还是不要改的好.
+
+对于静态方法其实和普通的方法一样,不需要对谁进行绑定,唯一的区别是调用的时候需要使用a.static_foo(x)或者A.static_foo(x)来调用.
+"""
