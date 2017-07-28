2891d9d75c93fcf025bb4ae5e0c99d0ba4feeca9
yu.xiang <yu.xiang@alibaba-inc.com>
2017-07-28 17:59:57 +0800

    upadate item

--- object_oriented_programming/private.py
+++ object_oriented_programming/private.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 
 
+# one example
 class Point:
     def __init__(self, x=0, y=0):
         self.__x, self.__y = x, y
@@ -18,3 +19,39 @@ except Exception as e:
     print e
 
 print p._Point__x
+
+
+# another example
+class Student(object):
+
+    def __init__(self, name, score):
+        self.__name = name
+        self.__score = score
+
+    def get_name(self):
+        return self.__name
+
+    def get_score(self):
+        return self.__score
+
+    def set_score(self, score):
+        if 0 <= score <= 100:
+            self.__score = score
+        else:
+            raise ValueError('bad score')
+
+    def get_grade(self):
+        if self.__score >= 90:
+            return 'A'
+        elif self.__score >= 60:
+            return 'B'
+        else:
+            return 'c'
+
+bart = Student('Bart Simpson', 59)
+print('bart.get_name() = ', bart.get_name())
+
+bart.set_score(60)
+print('bart.get_score() =', bart.get_score())
+
+print('DO NOT use bart._Student__name:', bart._Student__name)
