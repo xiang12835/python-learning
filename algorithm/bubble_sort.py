7d5f3cdd0cad045ac251799050e9b4b610685adc
yu.xiang <yu.xiang@alibaba-inc.com>
2017-08-04 13:44:42 +0800

    bubble

--- algorithm/bubble_sort.py
+++ algorithm/bubble_sort.py
@@ -13,3 +13,12 @@ def bubble_sort(lists):
             if lists[i] > lists[j]:
                 lists[i], lists[j] = lists[j], lists[i]
     return lists
+
+
+
+a = [6,5,4,3,2,1]
+for i in range(len(a)):
+    for j in range(len(a)-1):
+        if a[j] > a[j+1]:
+            a[j], a[j+1] = a[j+1], a[j]
+print a
