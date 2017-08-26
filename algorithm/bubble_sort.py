83174b0daf6af3bde5931c6e257ac4ada24efd22
yu.xiang <yu.xiang@alibaba-inc.com>
2017-08-26 14:57:43 +0800

    update bubble sort

--- algorithm/bubble_sort.py
+++ algorithm/bubble_sort.py
@@ -1,8 +1,17 @@
 # coding=utf-8
 
 
+"""
+    Bubble Sort
+    -----------
+    Time Complexity: O(n**2)
+    Space Complexity: O(1) Auxiliary
+    Stable: Yes
+"""
+
+
 # 冒泡排序
-def bubble_sort(lists):  # 小数上浮，每趟会在 i 处找到最小的数
+def bubble_sort(lists):  # 小数上浮 - 每趟会在 i 处找到最小的数
     """ 
     它重复地走访过要排序的数列，一次比较两个元素，如果他们的顺序错误就把他们交换过来。
     走访数列的工作是重复地进行直到没有再需要交换，也就是说该数列已经排序完成。
