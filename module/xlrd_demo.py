888fa415ef0eba77e3ca720f8eb7ca7594efcc43
yu.xiang <yu.xiang@alibaba-inc.com>
2018-01-20 18:38:52 +0800

    add xlrd_demo

---
+++ module/xlrd_demo.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+import xlrd
+import os
+
+
+def read_excel(file_name):
+
+    # 打开文件
+    workbook = xlrd.open_workbook(file_name)
+    # 根据sheet索引或者名称获取sheet内容
+    sheet1 = workbook.sheet_by_index(0)  # sheet索引从0开始
+    datas = []
+
+    for row in xrange(1, sheet1.nrows):
+        # print row
+        data = {
+            'order_id': str(sheet1.cell(row, 0).value).strip(),
+            'phone': str(int(sheet1.cell(row, 1).value)).strip(),
+            'express_code': str(int(sheet1.cell(row, 2).value)).strip(),
+        }
+        datas.append(data)
+
+    return datas
+
+
+if __name__ == "__main__":
+
+    f = os.path.join(os.path.dirname(__file__), "test.xlsx")
+
+    print read_excel(f)
