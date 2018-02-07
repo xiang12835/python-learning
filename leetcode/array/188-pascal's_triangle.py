7221626b2bdf1487f0a65a895b48cb53cb1ee5e4
yu.xiang <yu.xiang@alibaba-inc.com>
2018-02-08 05:21:35 +0800

    add 118. Pascal's Triangle

---
+++ leetcode/array/188-pascal's_triangle.py
@@ -0,0 +1,36 @@
+"""
+Given numRows, generate the first numRows of Pascal's triangle.
+
+For example, given numRows = 5,
+Return
+
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+
+"""
+
+
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        if numRows == 0:
+            return []
+
+        r = [[1]]
+        for i in xrange(1, numRows):
+            r.append(map(lambda x, y: x + y, [0] + r[-1], r[-1] + [0]))
+        return r
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print s.generate(3)
+
