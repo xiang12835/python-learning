cac11e37a6b9ab6bc24a98a5c4fc0182cf712a48
yu.xiang <yu.xiang@alibaba-inc.com>
2018-02-08 20:12:04 +0800

    update_rotate

--- leetcode/array/189. Rotate Array.py
+++ leetcode/array/189. Rotate Array.py
@@ -17,3 +17,14 @@ class Solution(object):
         """
         n = len(nums)
         nums[:] = nums[n - k:] + nums[:n - k]
+
+
+class Solution1(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        for i in xrange(k):
+            nums.insert(0, nums.pop())
