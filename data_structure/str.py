1ecb7293087a718585d19c5eb6851d107b544299
yu.xiang <yu.xiang@alibaba-inc.com>
2018-02-13 00:16:01 +0800

    count

--- data_structure/str.py
+++ data_structure/str.py
@@ -5,3 +5,11 @@ print "Hello {a}".format(a="Python")
 print "Hello %s" % "Python"
 
 print "Hello " + "Python"
+
+
+# Python count() 方法用于统计字符串里某个字符出现的次数。可选参数为在字符串搜索的开始与结束位置。
+str = "this is string example....wow!!!"
+sub = "i"
+print "str.count(sub, 4, 40) : ", str.count(sub, 4, 40)
+sub = "wow"
+print "str.count(sub) : ", str.count(sub)
