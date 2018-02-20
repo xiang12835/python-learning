2605958e68d2811774e3899f1892b03d1623980b
yu.xiang <yu.xiang@alibaba-inc.com>
2018-02-20 23:07:10 +0800

    update_replaceSpace

--- target_offer/4.replaceSpace.py
+++ target_offer/4.replaceSpace.py
@@ -1,6 +1,11 @@
 # -*- coding:utf-8 -*-
 
 
+""" 题目描述
+请实现一个函数，将一个字符串中的空格替换成“%20”。例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
+"""
+
+
 class Solution:
     # s 源字符串
     def replaceSpace1(self, s):
@@ -11,15 +16,15 @@ class Solution:
 
     def replaceSpace2(self, s):
         # 法二：创建新的字符串进行替换
-        res = ''
+        r = ''
         if type(s) != str:
             return
         for c in s:
             if c == ' ':
-                res += '%20'
+                r += '%20'
             else:
-                res += c
-        return res
+                r += c
+        return r
 
     def replaceSpace3(self, s):
         # 法三：逆向思维
