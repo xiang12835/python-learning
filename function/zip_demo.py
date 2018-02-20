1c8bb1b185cd743af8e3fb6b46f98fdf12d58a10
yu.xiang <yu.xiang@alibaba-inc.com>
2018-02-20 21:31:52 +0800

    update_zipped

--- function/zip_demo.py
+++ function/zip_demo.py
@@ -18,3 +18,13 @@ print lst
 
 dic = dict(lst)
 print dic
+
+
+
+a = [1,2,3]
+b = [4,5,6]
+c = [4,5,6,7,8]
+zipped = zip(a,b)     # 打包为元组的列表
+print zipped
+print zip(a,c)              # 元素个数与最短的列表一致
+print zip(*zipped)          # 与 zip 相反，可理解为解压，返回二维矩阵式
