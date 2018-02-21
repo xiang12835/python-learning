efb62e6b8507590e46e16559fd79cb6af823f9a1
yu.xiang <yu.xiang@alibaba-inc.com>
2018-02-21 20:41:13 +0800

    add pow

---
+++ function/pow.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+
+""" 描述
+pow() 方法返回 xy（x的y次方） 的值。
+"""
+
+import math  # 导入 math 模块
+
+print "math.pow(100, 2) : ", math.pow(100, 2)
+# 使用内置，查看输出结果区别
+print "pow(100, 2) : ", pow(100, 2)
+
+print "math.pow(100, -2) : ", math.pow(100, -2)
+print "math.pow(2, 4) : ", math.pow(2, 4)
+print "math.pow(3, 0) : ", math.pow(3, 0)
