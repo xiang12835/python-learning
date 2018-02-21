44c4c85b736241dbaebd4688b906ec408b56a990
yu.xiang <yu.xiang@alibaba-inc.com>
2018-02-21 12:33:34 +0800

    add_reConstructBinaryTree

--- target_offer/6.reConstructBinaryTree.py
+++ target_offer/6.reConstructBinaryTree.py
@@ -9,3 +9,27 @@
  """
 
 
+# -*- coding:utf-8 -*-
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    # 返回构造的TreeNode根节点
+    def reConstructBinaryTree(self, pre, tin):
+        # write code here
+        if not pre or not tin:
+            return None
+
+        root = TreeNode(pre[0])
+        index = 0
+        while index <= len(tin) - 1:
+            if tin[index] == pre[0]:
+                break
+            index += 1
+        root.left = self.reConstructBinaryTree(pre[1:index + 1], tin[:index])
+        root.right = self.reConstructBinaryTree(pre[index + 1:], tin[index + 1:])
+        return root
