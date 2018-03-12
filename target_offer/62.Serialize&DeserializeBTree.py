5a9fd1533aeb8ed091238a1402516081e2009b48
yu.xiang <yu.xiang@alibaba-inc.com>
2018-03-13 00:09:31 +0800

    add_Serialize

---
+++ target_offer/62.Serialize&DeserializeBTree.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+
+
+"""题目描述
+
+请实现两个函数，分别用来序列化和反序列化二叉树
+
+"""
+
+
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    def Serialize(self, root):
+        # write code here
+        pass
+
+    def Deserialize(self, s):
+        # write code here
+        pass
