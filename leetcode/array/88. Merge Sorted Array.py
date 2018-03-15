871d27f2e205eebfc49263f75dbeb439388f970f
yu.xiang <yu.xiang@alibaba-inc.com>
2018-03-16 00:49:11 +0800

    update_merge_list

--- leetcode/array/88. Merge Sorted Array.py
+++ leetcode/array/88. Merge Sorted Array.py
@@ -5,6 +5,10 @@ Note:
 You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2. The number of elements initialized in nums1 and nums2 are m and n respectively.
 """
 
+"""思路
+由于两个数组有序，考虑从后往前比较
+"""
+
 
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
@@ -25,3 +29,15 @@ class Solution(object):
 
         if n > 0:
             nums1[:n] = nums2[:n]
+
+
+class Solution1(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        return (nums1+nums2).sort()
