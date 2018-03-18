96118e643ec510c9f8c1257bf0a85ffa1d2ad056
yu.xiang <yu.xiang@alibaba-inc.com>
2018-03-18 21:26:13 +0800

    update_number

--- data_structure/number.py
+++ data_structure/number.py
@@ -3,11 +3,14 @@
 
 dec = 2147483647
 b = bin(dec)
+
+
 o = oct(dec)
 h = hex(dec)
 
 
 print "十进制数为：", dec
+print "转换为二进制为：", '{:b}'.format(dec)
 print "转换为二进制为：", b
 print "转换为八进制为：", o
 print "转换为十六进制为：", h
