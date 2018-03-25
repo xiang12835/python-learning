ba65383704e2cdf0afe20a1af80a1d4d7ffeaa47
yu.xiang <yu.xiang@alibaba-inc.com>
2018-03-26 00:06:06 +0800

    updatedel dum

--- target_offer/57.deleteDuplication.py
+++ target_offer/57.deleteDuplication.py
@@ -24,9 +24,24 @@ class Solution:
             return pHead
 
 
+class Solution1:
+    def deleteDuplication(self, pHead):
+        # write code here
+        if not pHead or not pHead.next:  # 只有0个或1个结点，则返回
+            return pHead
+
+        if pHead.val == pHead.next.val:  # 当前结点是重复结点
+            p = pHead.next
+            # while p and p.val == pHead.val:  # 跳过值与当前结点相同的全部结点,找到第一个与当前结点不同的结点
+            #     p = p.next
+            return self.deleteDuplication(p)  # 从第一个与当前结点不同的结点开始递归
+        else:  # 当前结点不是重复结点
+            pHead.next = self.deleteDuplication(pHead.next)  # 保留当前结点，从下一个结点开始递归
+            return pHead
+
 if __name__ == "__main__":
 
-    # {1,2,3,3,4,4,5}
+    # {1,2,3,3,3,4,4,5}
 
     head = ListNode(1)
 
@@ -46,10 +61,20 @@ if __name__ == "__main__":
 
 
 
-    s = Solution()
-    p = s.deleteDuplication(head)
+    # s = Solution()
+    # p = s.deleteDuplication(head)
+    #
+    # while p:
+    #     print p.val
+    #     p = p.next
+    #
+    # print "end"
 
-    while p:
-        print p.val
-        p = p.next
+    s1 = Solution1()
+    p1 = s1.deleteDuplication(head)
 
+    while p1:
+        print p1.val
+        p1 = p1.next
+
+    print "end"
