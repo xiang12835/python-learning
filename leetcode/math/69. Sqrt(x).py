22c8e9605365a7af925dea7ec1f60b7a97b622c5
yu.xiang <yu.xiang@alibaba-inc.com>
2018-03-27 23:54:52 +0800

    update mySqrt

--- leetcode/math/69. Sqrt(x).py
+++ leetcode/math/69. Sqrt(x).py
@@ -23,6 +23,13 @@ Explanation: The square root of 8 is 2.82842..., and since we want to return an 
 """
 
 
+"""思路解析
+
+一个数 x 的开方 sqrt 一定在 0 ~ x 之间，并且满足 sqrt == x / sqrt 。可以利用二分查找在 0 ~ x 之间查找 sqrt。
+
+"""
+
+
 class Solution(object):
     def mySqrt(self, x):
         """
@@ -33,3 +40,32 @@ class Solution(object):
         while r * r > x:
             r = (r + x / r) / 2
         return r
+
+
+class Solution1(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x < 1:
+            return x
+
+        l, r = 1, x
+        while l <= r:
+            mid = (l + r) / 2
+            sqrt = x / mid
+            if sqrt == mid:
+                return mid
+            elif sqrt < mid:
+                r = mid - 1
+            else:
+                l = mid + 1
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print s.mySqrt(100)
+
+    s1 = Solution1()
+    print s1.mySqrt(100)
