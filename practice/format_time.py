aab711c073f6942680f82a8b5ea5c2965fa18dde
yu.xiang <yu.xiang@alibaba-inc.com>
2018-03-29 10:17:51 +0800

    update_time

--- practice/format_time.py
+++ practice/format_time.py
@@ -16,3 +16,16 @@ result1 = "使用format打印：{}秒 = {}时{}分{}秒".format(num, hour, minut
 result2 = "使用字符串拼接打印：" + str(num) + "秒 = " + str(hour) + "时" + str(minute) + "分" + str(second) + "秒"
 print(result1)
 print(result2)
+
+
+
+def format_time(self):
+    h = self.use_time / 3600
+    m = self.use_time % 3600 / 60
+    s = self.use_time % 3600 % 60
+    if h:
+        return "{h}时{m}分{s}秒".format(h=h, m=m, s=s)
+    elif m:
+        return "{m}分{s}秒".format(m=m, s=s)
+    else:
+        return "{s}秒".format(s=s)
