2c66f840f079d0c6a1ab5ae2b58b74f1bc88a3eb
yu.xiang <yu.xiang@alibaba-inc.com>
2018-04-09 23:15:04 +0800

    reverseList

--- leetcode/linked_list/206. Reverse Linked List.py
+++ leetcode/linked_list/206. Reverse Linked List.py
@@ -30,3 +30,40 @@ class Solution(object):
         cur.next = pre
 
         return cur
+
+
+class Solution1(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head:
+            return None
+
+        pre = None
+        cur = head
+        while cur:
+            nxt = cur.next
+            cur.next = pre
+
+            pre = cur
+            cur = nxt
+
+        return pre
+
+
+class Solution2(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        return self._reverse(head)
+
+    def _reverse(self, node, pre=None):
+        if not node:
+            return pre
+        nxt = node.next
+        node.next = pre
+        return self._reverse(nxt, node)
