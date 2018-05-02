ae8a4142380935e75e1d60637e60fd347adf3a51
yu.xiang <yu.xiang@alibaba-inc.com>
2018-05-02 18:31:01 +0800

    update_binsearch

--- algorithm/bin_search.py
+++ algorithm/bin_search.py
@@ -15,3 +15,30 @@ def bin_search(l, n):
             right = mid - 1
 
     return -1
+
+
+""" 二分查找
+
+二分搜索算法的复杂度是对数级别的，意味着用bisect来搜索包含一百万个元素的列表，与用index来搜索包含14个元素的列表，所耗的时间差不多
+
+"""
+
+
+import time
+
+t0 = time.time()
+x = list(range(10**6))
+i = x.index(991234)
+print i
+
+t1 = time.time()
+print t1 - t0
+
+
+from bisect import bisect_left
+j = bisect_left(x, 991234)
+print j
+
+t2 = time.time()
+print t2 - t1
+
