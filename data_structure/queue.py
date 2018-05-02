b1d2fc29c4438ae5a3c282220e62e44862492a05
yu.xiang <yu.xiang@alibaba-inc.com>
2018-05-02 18:11:44 +0800

    update_queue

--- data_structure/queue.py
+++ data_structure/queue.py
@@ -32,3 +32,42 @@ class Queue():
             print("Queue is empty")
         else:
             qu.front -= 1
+
+
+""" 双向队列
+
+双向队列，头部或尾部插入或删除一个元素，只需要消耗常数级别的时间
+而list，而从list头部插入或删除一个元素，只需要消耗线性级别的时间，比较慢
+从list尾部插入或删除一个元素，只需要消耗常数级别的时间
+
+"""
+from collections import deque
+
+fifo = deque()
+fifo.append(1)
+print fifo
+fifo.popleft()
+print fifo
+
+
+""" 优先级队列（堆队列）
+
+
+
+"""
+from heapq import heappush, heappop, nsmallest
+
+a = []
+
+heappush(a, 5)
+heappush(a, 3)
+heappush(a, 7)
+heappush(a, 4)
+
+print a
+print nsmallest(1, a)
+assert a[0] == nsmallest(1, a)[0] == 3
+
+print heappop(a), heappop(a), heappop(a), heappop(a)
+print a
+
