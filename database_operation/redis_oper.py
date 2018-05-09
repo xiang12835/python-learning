f05e927d110a655a4575d01b99174d760c7901c6
yu.xiang <yu.xiang@alibaba-inc.com>
2018-05-09 22:39:04 +0800

    update_redis_oper

--- database_operation/redis_oper.py
+++ database_operation/redis_oper.py
@@ -1,21 +1,30 @@
 # coding=utf-8
 
 
-# import redis
+import redis
 
 
-# r = redis.StrictRedis(host='localhost', port=6379, db=0)
-# r.set('foo', 'bar')
-# r.get('foo')
-#
-# print r.get('foo')
-#
-# pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
-# r = redis.StrictRedis(connection_pool=pool)
-#
-# keys = r.keys()
-# print type(keys)
-# print keys
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+r.set('foo', 'bar')
+r.get('foo')
+
+print r.get('foo')
+
+pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
+r = redis.StrictRedis(connection_pool=pool)
+
+keys = r.keys()
+print type(keys)
+print keys
+
+
+r = redis.Redis(host='localhost', port=6379, db=0)
+
+print r.keys()
+# print r.llen(r)
+
+r.delete("foo")
+print r.keys()
 
 
 
