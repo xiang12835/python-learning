c6831ca298b9898be9bb557ab68bd06f5a04060d
yu.xiang <yu.xiang@alibaba-inc.com>
2018-07-08 00:32:17 +0800

    update re

--- regex/re_demo.py
+++ regex/re_demo.py
@@ -12,6 +12,18 @@ def test_search():
     return r
 
 
-if __name__ == "__main__":
-    print test_search()
 
+def replace_subject_image_url(mat):
+    if mat:
+        return 'src="%s"' % ("http://115.159.122.45" + "/" + mat.group(1))
+
+def render_subject_img(value):
+    return re.sub(r'src=[\"\'](.*?)[\"\']', replace_subject_image_url, value)
+
+
+
+if __name__ == "__main__":
+    # print test_search()
+
+    val = """<span class="desClass">中共中央办公厅于2017年3月印发了《关于推进“两学一做”学习教育常态化制度化的意见》，意见指出要把____作为查找和解决问题的重要途径，注意听取群众的意见和反馈，抓早抓小、防微杜渐。<img src="http://img.winlesson.com/images/f0444e74c1f8ca523d793e8161c25099.png" _src="http://img.winlesson.com/images/f0444e74c1f8ca523d793e8161c25099.png" style="user-select: none;"/></span>"""
+    print render_subject_img(val)
