bf62b5f0eaa3cf6c84dae0f5c54f6976837a7444
yu.xiang <yu.xiang@alibaba-inc.com>
2018-10-30 00:04:16 +0800

    update_insert_sort

--- algorithm/insert_sort.py
+++ algorithm/insert_sort.py
@@ -2,7 +2,7 @@
 
 
 # 插入排序
-def insert_sort(lst=[]):
+def insert_sort(a):
     """
     插入排序的基本操作就是将一个数据插入到已经排好序的有序数据中，从而得到一个新的、个数加一的有序数据，算法适用于少量数据的排序，时间复杂度为O(n^2)。
     是稳定的排序方法。插入算法把要排序的数组分成两部分：第一部分包含了这个数组的所有元素，但将最后一个元素除外（让数组多一个空间才有插入的位置），
@@ -10,13 +10,19 @@ def insert_sort(lst=[]):
     :param lst:
     :return:
     """
-    count = len(lst)
-    for i in range(1, count):
-        val = lst[i]  # i指向带插元素
+    n = len(a)
+    for i in range(1, n):
+        val = a[i]  # i指向带插元素
         j = i - 1  # j指向有序元素
         while j >= 0:
-            if lst[j] > val:
-                lst[j + 1] = lst[j]  # j指向的有序元素后移
-                lst[j] = val
+            if a[j] > val:
+                a[j + 1] = a[j]  # j指向的有序元素后移
+            else:
+                break
             j -= 1
-    return lst
+        a[j+1] = val  # 插入位置
+    return a
+
+a = [3, 5, 4, 6, 2, 1]
+print insert_sort(a)
+
