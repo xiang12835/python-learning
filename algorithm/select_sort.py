b8abffe9915991bb047af2cc3082229df23880f6
yu.xiang <yu.xiang@alibaba-inc.com>
2018-10-30 00:22:31 +0800

    update_select_sort

--- algorithm/select_sort.py
+++ algorithm/select_sort.py
@@ -2,7 +2,7 @@
 
 
 # 选择排序
-def select_sort(lst=[]):
+def select_sort(a):
     """
     基本思想：第1趟，在待排序记录r1 ~ r[n]中选出最小的记录，将它与r1交换；
     第2趟，在待排序记录r2 ~ r[n]中选出最小的记录，将它与r2交换；
@@ -11,11 +11,15 @@ def select_sort(lst=[]):
     :param lst: 
     :return: 
     """
-    count = len(lst)
-    for i in range(0, count):
+    n = len(a)
+    for i in range(0, n):
         min = i
-        for j in range(i + 1, count):  # 从待排记录中选出最小的记录
-            if lst[min] > lst[j]:
+        for j in range(i + 1, n):  # 从待排记录中选出最小的记录
+            if a[min] > a[j]:
                 min = j
-        lst[min], lst[i] = lst[i], lst[min]  # 将最小的记录与待排序记录中的第一个记录进行交换
-    return lst
+        a[min], a[i] = a[i], a[min]  # 将最小的记录与待排序记录中的第一个记录进行交换
+    return a
+
+
+a = [3, 5, 4, 6, 2, 1]
+print select_sort(a)
