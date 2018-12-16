6313b580a899ec4dd408a549eb112ea96b2c24d2
yu.xiang <yu.xiang@alibaba-inc.com>
2018-12-16 17:54:05 +0800

    add_52

---
+++ leetcode/bit/52. N-Queens II.py
@@ -0,0 +1,52 @@
+# coding=utf-8
+
+"""
+The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
+
+Given an integer n, return the number of distinct solutions to the n-queens puzzle.
+
+Example:
+
+Input: 4
+Output: 2
+Explanation: There are two distinct solutions to the 4-queens puzzle as shown below.
+[
+ [".Q..",  // Solution 1
+  "...Q",
+  "Q...",
+  "..Q."],
+
+ ["..Q.",  // Solution 2
+  "Q...",
+  "...Q",
+  ".Q.."]
+]
+
+"""
+
+
+class Solution(object):
+    def totalNQueens(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n < 1:
+            return []
+        self.count = 0
+        self.DFS(n, 0, 0, 0, 0)
+        return self.count
+
+    def DFS(self, n, row, col, pie, na):
+        # 递归终止条件
+        if row >= n:
+            self.count += 1
+            return
+
+        bits = (~(col | pie | na)) & ((1 << n) - 1)  # 得到当前所有的空位
+
+        while bits:
+            p = bits & -bits  # 取到最低位的1
+            self.DFS(n, row + 1, col | p, (pie | p) << 1, (na | p) >> 1)
+            bits = bits & (bits - 1)  # 去掉最低位的1
+
