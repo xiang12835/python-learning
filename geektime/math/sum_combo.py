bdffff2787afe3170000aa0da8f8129e8a110500
yu.xiang <yu.xiang@alibaba-inc.com>
2019-01-06 21:59:12 +0800

    update_get_sum_combo

--- geektime/math/sum_combo.py
+++ geektime/math/sum_combo.py
@@ -15,16 +15,18 @@ def get_sum_combo(total_reword, result=[]):
     Returns: void
 
     """
-    if total_reword == 0:
+    if total_reword == 0:  # 证明它是满足条件的解，结束嵌套调用，输出解
+
         print(result)
         return
-    elif total_reword < 0:
+    elif total_reword < 0:  # 证明它不是满足条件的解，不输出
         return
     else:
         for i in range(len(rewards)):
-            new_result = copy.copy(result)
-            new_result.append(rewards[i])
-            get_sum_combo(total_reword - rewards[i], new_result)
+            new_result = copy.copy(result)  # 由于有 4 种情况，需要 clone 当前的解并传入被调用的函数
+
+            new_result.append(rewards[i])  # 记录当前的选择，解决一点问题
+            get_sum_combo(total_reword - rewards[i], new_result)  # 剩下的问题，留给嵌套调用去解决
 
 
 if __name__ == "__main__":
