9b0681635b03e82df5a3bb446ef91e4988ab1d33
yu.xiang <yu.xiang@alibaba-inc.com>
2019-02-10 17:19:31 +0800

    update_majority

--- leetcode/array/169. Majority Element.py
+++ leetcode/array/169. Majority Element.py
@@ -1,4 +1,4 @@
-# coding = utf-8
+# coding=utf-8
 
 """
 Given an array of size n, find the majority element. The majority element is the element that appears more than [ n/2 ] times.
@@ -12,11 +12,31 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: int
+
+        排序
         """
         return sorted(nums)[len(nums) / 2]
 
 
 class Solution1(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+
+        字典记录出现次数，取次数最多对应的key即可
+
+        """
+        lookup = {}
+        for num in nums:
+            lookup[num] = lookup.get(num, 0) + 1
+        max_occur = max(lookup.values())
+        for key in lookup:
+            if lookup[key] == max_occur:
+                return key
+
+
+class Solution2(object):
     def majorityElement(self, nums):
         """
         :type nums: List[int]
@@ -34,3 +54,31 @@ class Solution1(object):
                     cnt = 1
 
         return nums[idx]
+
+
+class Solution3(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+
+这个问题有一个很出名的算法
+
+Boyer-Moore众数(majority number) 问题
+
+在数组中找到两个不相同的元素并删除它们，不断重复此过程，直到数组中元素都相同，那么剩下的元素就是主要元素。
+
+这个算法的妙处在于不直接删除数组中的元素，而是利用一个计数变量.
+        """
+        count = 0
+        tmp = None
+        for n in nums:
+            if count == 0:
+                tmp = n
+
+            if n == tmp:
+                count += 1
+            else:
+                count -= 1
+
+        return tmp
