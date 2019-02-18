474c1e043ab8e85699f28346f674312b6c9f3f0d
yu.xiang <yu.xiang@alibaba-inc.com>
2019-02-18 15:40:29 +0800

    add_input

---
+++ basic_syntax/input.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+
+""" 简述你对 input()函数的理解?
+
+在 Python3 中，input()获取用户输入，不论用户输入的是什么，获取到的都是字符串类型的。
+在 Python2 中有 raw_input()和 input(), raw_input()和 Python3 中的 input()作用是一样的， input()输入的是什么数据类型的，获取到的就是什么数据类型的。
+
+"""
