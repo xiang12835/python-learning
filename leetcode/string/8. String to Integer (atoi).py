550831f02810907ac5c469f3aa2e3bf91e6fe510
yu.xiang <yu.xiang@alibaba-inc.com>
2019-02-25 11:01:49 +0800

    add_atoi

--- leetcode/string/8. String to Integer (atoi).py
+++ leetcode/string/8. String to Integer (atoi).py
@@ -1,13 +1,9 @@
+# coding=utf-8
+
+
 """
-Implement atoi to convert a string to an integer.
 
-Hint: Carefully consider all possible input cases. If you want a challenge, please do not see below and ask yourself what are the possible input cases.
-
-Notes: It is intended for this problem to be specified vaguely (ie, no given input specs). You are responsible to gather all the input requirements up front.
-
-
-
-Requirements for atoi:
+Implement atoi which converts a string to an integer.
 
 The function first discards as many whitespace characters as necessary until the first non-whitespace character is found. Then, starting from this character, takes an optional initial plus or minus sign followed by as many numerical digits as possible, and interprets them as a numerical value.
 
@@ -15,7 +11,45 @@ The string can contain additional characters after those that form the integral 
 
 If the first sequence of non-whitespace characters in str is not a valid integral number, or if no such sequence exists because either str is empty or it contains only whitespace characters, no conversion is performed.
 
-If no valid conversion could be performed, a zero value is returned. If the correct value is out of the range of representable values, INT_MAX (2147483647) or INT_MIN (-2147483648) is returned.
+If no valid conversion could be performed, a zero value is returned.
+
+Note:
+
+    Only the space character ' ' is considered as whitespace character.
+    Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. If the numerical value is out of the range of representable values, INT_MAX (231 − 1) or INT_MIN (−231) is returned.
+
+Example 1:
+
+Input: "42"
+Output: 42
+
+Example 2:
+
+Input: "   -42"
+Output: -42
+Explanation: The first non-whitespace character is '-', which is the minus sign.
+             Then take as many numerical digits as possible, which gets 42.
+
+Example 3:
+
+Input: "4193 with words"
+Output: 4193
+Explanation: Conversion stops at digit '3' as the next character is not a numerical digit.
+
+Example 4:
+
+Input: "words and 987"
+Output: 0
+Explanation: The first non-whitespace character is 'w', which is not a numerical
+             digit or a +/- sign. Therefore no valid conversion could be performed.
+
+Example 5:
+
+Input: "-91283472332"
+Output: -2147483648
+Explanation: The number "-91283472332" is out of the range of a 32-bit signed integer.
+             Thefore INT_MIN (−231) is returned.
+
 
 """
 
@@ -27,28 +61,79 @@ class Solution(object):
         :rtype: int
         """
 
-        INT_MAX = 2147483647
-        INT_MIN = -2147483648
+        MAX_INT = pow(2, 31) - 1
+        MIN_INT = -1 * pow(2, 31)
 
         s = str.strip()
-        sign = 1
         if not s:
             return 0
-        if s[0] in ["+", "-"]:
-            if s[0] == "-":
+
+        sign = 1
+
+        if s[0] in ['+', '-']:  # 符号判断
+            if s[0] == '-':
                 sign = -1
+
             s = s[1:]
+
         r = 0
         for c in s:
             if c.isdigit():
-                r = r * 10 + int(c)
+                r = 10 * r + int(c)
             else:
                 break
-        r *= sign
-        if r > INT_MAX:
-            return INT_MAX
-        if r < INT_MIN:
-            return INT_MIN
+
+        r = sign * r
+
+        if r > MAX_INT:
+            r = MAX_INT
+        if r < MIN_INT:
+            r = MIN_INT
+
+        return r
+
+
+class Solution1(object):
+    def myAtoi(self, str):
+        """
+        :type str: str
+        :rtype: int
+        """
+
+        MAX_INT = pow(2, 31) - 1
+        MIN_INT = -1 * pow(2, 31)
+
+        s = str.strip()
+        if not s:
+            return 0
+
+        sign = 1
+        tmp = s
+
+        for c in s:  # 符号判断
+            if c in ['+', '-']:
+                if c == '-':
+                    sign = -1
+                else:
+                    sign = 1
+
+                tmp = tmp[1:]
+
+        s = tmp
+
+        r = 0
+        for c in s:
+            if c.isdigit():
+                r = 10 * r + int(c)
+            else:
+                break
+
+        r = sign * r
+
+        if r > MAX_INT:
+            r = MAX_INT
+        if r < MIN_INT:
+            r = MIN_INT
 
         return r
 
