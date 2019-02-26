d76e35e98f5ea152cb90ea3655d1d263d9e4ea8e
yu.xiang <yu.xiang@alibaba-inc.com>
2019-02-26 14:11:02 +0800

    update_3sum

--- leetcode/array/15. 3Sum.py
+++ leetcode/array/15. 3Sum.py
@@ -27,37 +27,27 @@ class Solution(object):
 
         排序算法；左右碰撞
         """
-        n = len(nums)
-        ans = []
+        n, ans = len(nums), []
 
         nums.sort()
 
         for i in xrange(n):
-            if i > 0 and nums[i] == nums[i - 1]:
+            if i > 0 and nums[i] == nums[i - 1]:  # 因为i=0这个元素会直接往下执行；去重复
                 continue
 
-            l = i + 1
-            r = n - 1
-
+            l, r = i + 1, n - 1
             while l < r:
                 tmp = nums[i] + nums[l] + nums[r]
-
                 if tmp == 0:
-                    ans.append((nums[i], nums[l], nums[r]))
+                    ans.append([nums[i], nums[l], nums[r]])
                     l += 1
                     r -= 1
-
-                    while l < r and nums[l] == nums[l - 1]:
+                    while l < r and nums[l] == nums[l - 1]:  # 去重复
                         l += 1
-                    while l < r and nums[r] == nums[r + 1]:
+                    while l < r and nums[r] == nums[r + 1]:  # 去重复
                         r -= 1
-
                 elif tmp > 0:
                     r -= 1
                 else:
                     l += 1
-
         return ans
-
-
-
