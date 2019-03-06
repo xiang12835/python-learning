a2606cd13c0d621fb3238362d742bb38ee10fe42
yu.xiang <yu.xiang@alibaba-inc.com>
2019-03-06 19:49:21 +0800

    add344

--- leetcode/string/344. Reverse String.py
+++ leetcode/string/344. Reverse String.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 """
 Write a function that takes a string as input and returns the string reversed.
 
@@ -13,3 +14,23 @@ class Solution(object):
         :rtype: str
         """
         return s[::-1]
+
+
+class Solution1(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        左右对撞
+        """
+
+        ss = list(s)
+        n = len(s)
+        l = 0
+        r = n - 1
+        while l < r:
+            ss[l], ss[r] = ss[r], ss[l]
+            l += 1
+            r -= 1
+
+        return ''.join(ss)
