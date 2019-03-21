6afc41f976e123106b4d04b3f04dc44979862777
yu.xiang <yu.xiang@alibaba-inc.com>
2019-03-21 15:26:35 +0800

    update_bsearch

--- algorithm/bin_search.py
+++ algorithm/bin_search.py
@@ -1,12 +1,58 @@
 # coding=utf-8
 
+"""
+
+// 二分查找的循环实现
+
+public int bsearch(int[] a, int n, int value) {
+  int low = 0;
+  int high = n - 1;
+
+  while (low <= high) {
+    int mid = (low + high) / 2;
+    if (a[mid] == value) {
+      return mid;
+    } else if (a[mid] < value) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+
+  return -1;
+}
+
+
+
+
+// 二分查找的递归实现
+public int bsearch(int[] a, int n, int val) {
+  return bsearchInternally(a, 0, n - 1, val);
+}
+
+private int bsearchInternally(int[] a, int low, int high, int value) {
+  if (low > high) return -1;
+
+  int mid =  low + ((high - low) >> 1);
+  if (a[mid] == value) {
+    return mid;
+  } else if (a[mid] < value) {
+    return bsearchInternally(a, mid+1, high, value);
+  } else {
+    return bsearchInternally(a, low, mid-1, value);
+  }
+}
+
+"""
+
 
 def bin_search(l, n):
     # Time: O(log(n))
     left = 0
     right = len(l) - 1
     while left <= right:
-        mid = (left + right) // 2
+        mid = left + (right - left) // 2
+        # mid = left + (right - left)>>2
         if n == l[mid]:
             return mid
         elif n > l[mid]:
@@ -15,6 +61,26 @@ def bin_search(l, n):
             right = mid - 1
 
     return -1
+
+
+def bin_search1(l, n):
+    return _recursion(l, 0, len(l) - 1, n)
+
+
+def _recursion(l, left, right, n):
+    if left > right:
+        return -1
+
+    mid = left + (right - left) >> 2
+
+    if n == l[mid]:
+        return mid
+    elif n > l[mid]:
+        return _recursion(l, mid+1, right, n)
+    else:
+        return _recursion(l, left, mid-1, n)
+
+
 
 
 """ 二分查找
