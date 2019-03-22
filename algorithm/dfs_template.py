e3f088f5a0e48b929843db380ac3dc928aa83d14
yu.xiang <yu.xiang@alibaba-inc.com>
2019-03-22 11:40:55 +0800

    add_dfs

---
+++ algorithm/dfs_template.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+
+
+def dfs(node):
+    # 递归写法
+    visited = set()
+    visited.add(node)
+
+    # process current node here
+
+    for next_node in node.children():
+        if not not next_node in visited:
+            dfs(next_node)
+
+
+def dfs_stack(self, tree):
+    if tree.root is None:
+        return []
+
+    visited = set()
+    stack = [tree.node]
+
+    while stack:
+        node = stack.pop()
+        visited.add(node)
+
+        process(node)
+
+        nodes = generate_related_nodes(node)
+        stack.push(nodes)
+
+    # other processing work
