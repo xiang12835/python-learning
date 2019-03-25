06ca0bab0536417205b5c5fb2a856b204402b61f
yu.xiang <yu.xiang@alibaba-inc.com>
2019-03-25 23:55:56 +0800

    can_jump

---
+++ geektime/algorithm_interview/canJump.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+
+"""
+
+7、给定一个非负整数数组，你最初位于数组的第一个位置。
+数组中的每个元素代表你在该位置可以跳跃的最大长度。
+你的目标是使用最少的跳跃次数到达数组的最后一个位置。
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+Each element in the array represents your maximum jump length at that position. Your goal is to reach the last index in the minimum number of jumps.
+
+"""
+
