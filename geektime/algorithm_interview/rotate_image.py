a77b7110d378a7af0f2ae5e40b9beda67ac1bacc
yu.xiang <yu.xiang@alibaba-inc.com>
2019-04-03 19:58:52 +0800

    add_48

---
+++ geektime/algorithm_interview/rotate_image.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+
+""" 48. 旋转图像
+
+8、给定一个 n × n 的二维矩阵表示一个图像。
+将图像顺时针旋转 90 度。
+You are given an n x n 2D matrix representing an image. Rotate the image by 90 degrees (clockwise).
+
+"""
+
+
+
