9941e4dcd64c6cb49531e95c053eff314d7bde7d
yu.xiang <yu.xiang@alibaba-inc.com>
2019-04-05 10:15:38 +0800

    update_283

--- leetcode/array/283. Move Zeroes.py
+++ leetcode/array/283. Move Zeroes.py
@@ -44,3 +44,26 @@ class Solution1(object):
         for j in xrange(idx, len(nums)):
             nums[idx] = 0
             idx += 1
+
+
+class Solution2(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+
+        快慢指针
+        """
+        slow = 0
+        fast = 0
+
+        n = len(nums)
+        while fast < n:
+            if nums[fast] != 0:
+                nums[slow] = nums[fast]
+                slow += 1
+            fast += 1
+
+        while slow < n:
+            nums[slow] = 0
+            slow += 1
