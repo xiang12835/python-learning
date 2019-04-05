a19399eac814384be1a33bb9076a1af628c18e8d
yu.xiang <yu.xiang@alibaba-inc.com>
2019-04-05 14:43:55 +0800

    add_215

--- leetcode/array/215. Kth Largest Element in an Array.py
+++ leetcode/array/215. Kth Largest Element in an Array.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 """
 Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
 
@@ -17,3 +19,26 @@ class Solution(object):
         :rtype: int
         """
         return sorted(nums, reverse=True)[k-1]
+
+
+class Solution1(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+
+        现在打算只维护一个容量为k的最大堆，最终堆顶的值就是结果
+
+        """
+
+        from heapq import heappush, heappop
+
+        n = len(nums)
+        heap = []
+        for i in xrange(n):
+            heappush(heap, nums[i])
+            if len(heap) > k:
+                heappop(heap)
+
+        return heap[0]
