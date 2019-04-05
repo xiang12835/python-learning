eb75fd31839646dcfbedbf3f6054a7fc000dc37b
yu.xiang <yu.xiang@alibaba-inc.com>
2019-04-05 17:44:01 +0800

    add_239

---
+++ leetcode/array/239. Sliding Window Maximum.py
@@ -0,0 +1,47 @@
+# coding=utf-8
+
+"""
+
+Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position. Return the max sliding window.
+
+Example:
+
+Input: nums = [1,3,-1,-3,5,3,6,7], and k = 3
+Output: [3,3,5,5,6,7]
+Explanation:
+
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+Note:
+You may assume k is always valid, 1 ≤ k ≤ input array's size for non-empty array.
+
+
+"""
+
+
+class Solution(object):
+    def maxSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        if not nums:
+            return []
+
+        n = len(nums)
+        r = []
+        for i in xrange(n - k + 1):
+            tmp = nums[i:i + k]
+            r.append(max(tmp))
+
+        return r
+
+
+
