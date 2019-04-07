c434c92c7a486044c4338233769c2e3699176ba8
yu.xiang <yu.xiang@alibaba-inc.com>
2019-04-08 00:35:11 +0800

    add_22

---
+++ leetcode/recursion/22. Generate Parentheses.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+
+
+"""
+
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+For example, given n = 3, a solution set is:
+
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+
+"""
+
+
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+
+        left 已经使用了的左括号的个数
+        right 已经使用了的右括号的个数
+
+        """
+        self.l = []
+        self._gen(0, 0, n, "")
+        return self.l
+
+    def _gen(self, left, right, n, result):
+        if left == n and right == n:
+            self.l.append(result)
+        return
+
+        if left < n:
+            self._gen(left + 1, right, n, result + '(')
+
+        if right < n and right < left:
+            self._gen(left, right + 1, n, result + ')')
