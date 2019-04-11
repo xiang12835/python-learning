30350b61fadc5896b1315056fe7d6157029c2fb5
yu.xiang <yu.xiang@alibaba-inc.com>
2019-04-11 18:59:29 +0800

    update binsearch

--- algorithm/bin_search.py
+++ algorithm/bin_search.py
@@ -310,3 +310,24 @@ print j
 t2 = time.time()
 print t2 - t1
 
+
+
+l = [2,4,5,7,8]
+import bisect
+print bisect.bisect(l, 5)
+print bisect.bisect_left(l, 5)
+print bisect.bisect_right(l, 5)
+
+print l
+
+
+
+def grade(score, breakpoints=[60, 70, 80, 90], grades='FDCBA'):
+    # 计算分数等级
+    i = bisect.bisect(breakpoints, score)
+    return grades[i]
+
+
+print [grade(score) for score in [33, 99, 77, 70, 89, 90, 100]]
+
+
