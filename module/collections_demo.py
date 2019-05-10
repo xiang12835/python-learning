fde6ca95e89881da27320a523e7708ad02516e11
yu.xiang <yu.xiang@alibaba-inc.com>
2019-05-11 06:47:48 +0800

    update_collections

--- module/collections_demo.py
+++ module/collections_demo.py
@@ -103,3 +103,18 @@ print Counter('programming')
 
 nums = [1, 2, 3, 4, 5]
 print Counter(nums)
+
+
+"""
+找出序列中出现次数最多的元素
+"""
+from collections import Counter
+
+words = [
+    'look', 'into', 'my', 'eyes', 'look', 'into', 'my', 'eyes',
+    'the', 'eyes', 'the', 'eyes', 'the', 'eyes', 'not', 'around',
+    'the', 'eyes', "don't", 'look', 'around', 'the', 'eyes',
+    'look', 'into', 'my', 'eyes', "you're", 'under'
+]
+counter = Counter(words)
+print counter.most_common(3)
