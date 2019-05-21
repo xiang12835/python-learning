54c493b3ddeb7fdefdde4407455acb08bbc74d9c
yu.xiang <yu.xiang@alibaba-inc.com>
2019-05-21 20:46:53 +0800

    update_client

--- network_programming/tcp_download_file_with_thread/client.py
+++ network_programming/tcp_download_file_with_thread/client.py
@@ -12,16 +12,16 @@ def main():
     client = socket()
     client.connect(('192.168.1.102', 8089))
     # 定义一个保存二进制数据的对象
-    in_data = bytes()
+    received_data = bytes()
     # 由于不知道服务器发送的数据有多大每次接收1024字节
     data = client.recv(1024)
     while data:
         # 将收到的数据拼接起来
-        in_data += data
+        received_data += data
         data = client.recv(1024)
     # 将收到的二进制数据解码成JSON字符串并转换成字典
     # loads函数的作用就是将JSON字符串转成字典对象
-    my_dict = loads(in_data.decode('utf-8'))
+    my_dict = loads(received_data.decode('utf-8'))
     filename = my_dict['filename']
     filedata = my_dict['filedata'].encode('utf-8')
 
