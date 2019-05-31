072365c1d0fca1d36db194ea8d38f68579fd37cb
yu.xiang <yu.xiang@alibaba-inc.com>
2019-05-31 15:48:43 +0800

    update_16

--- leetcode/array/16. 3Sum Closest.py
+++ leetcode/array/16. 3Sum Closest.py
@@ -14,35 +14,48 @@ The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
 """
 
 
-
 class Solution(object):
     def threeSumClosest(self, nums, target):
         """
         :type nums: List[int]
-        :rtype: List[List[int]]
+        :type target: int
+        :rtype: int
         """
-        n, res, diff = len(nums), None, float('inf')
+        n = len(nums)
+        ans = None
+        diff = float('inf')
+
         nums.sort()
-        for i in range(n):
-            if i > 0 and nums[i] == nums[i-1]:
+
+        for i in xrange(n):
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            l, r = i+1, n-1
+
+            l = i + 1
+            r = n - 1
             while l < r:
                 tmp = nums[i] + nums[l] + nums[r]
-                if tmp == target:
-                    return target
-                elif tmp > target:
+
+                if tmp > target:
                     r -= 1
-                    if abs(tmp-target) < diff:
-                        diff = abs(tmp-target)
-                        res = tmp
-                    while l < r and nums[r] == nums[r+1]:
+                    if abs(tmp - target) < diff:
+                        diff = abs(tmp - target)
+                        ans = tmp
+
+                    while l < r and nums[r] == nums[r + 1]:
                         r -= 1
-                else:
+
+                elif tmp < target:
                     l += 1
-                    if abs(tmp-target) < diff:
-                        diff = abs(tmp-target)
-                        res = tmp
-                    while l < r and nums[l] == nums[l-1]:
+                    if abs(tmp - target) < diff:
+                        diff = abs(tmp - target)
+                        ans = tmp
+
+                    while l < r and nums[l] == nums[l - 1]:
                         l += 1
-        return res
+
+                else:
+
+                    return target
+
+        return ans
