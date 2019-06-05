15c313c69cfcb4b636f4e15c52aa660bba2297cc
yu.xiang <yu.xiang@alibaba-inc.com>
2019-06-05 18:29:28 +0800

    add_285

---
+++ leetcode/tree/285. Inorder Successor in BST.py
@@ -0,0 +1,58 @@
+# coding=utf-8
+
+"""
+Given a binary search tree and a node in it, find the in-order successor of that node in the BST.
+
+The successor of a node p is the node with the smallest key greater than p.val.
+
+
+
+Input: root = [2,1,3], p = 1
+Output: 2
+Explanation: 1's in-order successor node is 2. Note that both p and the return value is of TreeNode type.
+
+Input: root = [5,3,6,2,4,null,null,1], p = 6
+Output: null
+Explanation: There is no in-order successor of the current node, so the answer is null.
+
+
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def inorderSuccessor(self, root, p):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :rtype: TreeNode
+        """
+
+        def minNode(node):
+            while node.left:
+                node = node.left
+            return node
+
+        def searchP(root, p):
+            if not root or root.val == p.val:
+                return None
+            else:
+                succ = None
+                while root != None and p.val != root.val:
+                    if p.val < root.val:
+                        succ = root
+                        root = root.left
+                    else:
+                        root = root.right
+                return succ
+
+        if p.right:
+            return minNode(p.right)
+        else:
+            return searchP(root, p)
