fddf1963a49151d8ca2103bdc36bc31d37b1100e
yu.xiang <yu.xiang@alibaba-inc.com>
2019-06-15 11:29:06 +0800

    update_61

--- target_offer/tree/61.Serialize&DeserializeBTree.py
+++ target_offer/tree/61.Serialize&DeserializeBTree.py
@@ -8,16 +8,33 @@
 """
 
 
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
 class Solution:
+    def __init__(self):
+        self.idx = -1
+
     def Serialize(self, root):
         # write code here
-        pass
+        if not root:
+            return '#,'
+        return str(root.val) + ',' + self.Serialize(root.left) + self.Serialize(root.right)
 
     def Deserialize(self, s):
         # write code here
-        pass
+        self.idx += 1
+        l = s.split(',')
+
+        if self.idx >= len(s):
+            return
+
+        root = None
+        if l[self.idx] != '#':
+            root = TreeNode(int(l[self.idx]))
+            root.left = self.Deserialize(s)
+            root.right = self.Deserialize(s)
+        return root
