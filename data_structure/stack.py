995e14a92575a04a20ed3bdaf07affa8ac542d98
yu.xiang <yu.xiang@alibaba-inc.com>
2019-07-01 18:35:30 +0800

    update_stack

--- data_structure/stack.py
+++ data_structure/stack.py
@@ -31,3 +31,70 @@ class Stack():
             print "Stack is empty"
         else:
             st.top=st.top-1
+
+
+
+class RightStack():
+    def __init__(st,size):
+        st.stack=[]
+        st.size=size
+        st.top=-1
+
+    def empty(st):
+        if st.top==-1:
+            return True
+        else:
+            return False
+
+    def full(st):
+        if st.top==st.size:
+            return True
+        else:
+            return False
+
+    def r_push(st,data):
+        if st.full():
+            print "RightStack is full"
+        else:
+            st.stack.append(data)
+            st.top=st.top+1
+
+    def r_pop(st):
+        if st.empty():
+            print "RightStack is empty"
+        else:
+            st.top=st.top-1
+
+
+class LeftStack():
+    def __init__(st,size):
+        st.stack=[]
+        st.size=size
+        st.top=-1
+
+    def empty(st):
+        if st.top==-1:
+            return True
+        else:
+            return False
+
+    def full(st):
+        if st.top==st.size:
+            return True
+        else:
+            return False
+
+    def l_push(st,data):
+        if st.full():
+            print "LeftStack is full"
+        else:
+            st.stack.insert(0, data)
+            st.top=st.top+1
+
+    def l_pop(st):
+        if st.empty():
+            print "LeftStack is empty"
+        else:
+            for i in xrange(1, len(st.stack)):
+                st.stack[i-1] = st.stack[i]
+            st.top=st.top-1
