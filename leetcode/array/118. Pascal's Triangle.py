9831e48bd911119ae1bfffd8985b76068f302f54
yu.xiang <yu.xiang@alibaba-inc.com>
2020-02-01 21:50:38 +0800

    update_118

--- leetcode/array/118. Pascal's Triangle.py
+++ leetcode/array/118. Pascal's Triangle.py
@@ -30,6 +30,24 @@ class Solution(object):
         return r
 
 
+class Solution1(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        if numRows == 0:
+            return []
+
+        r = [[1]]
+        for i in range(1, numRows):
+            first_list = [0] + r[-1]
+            second_list = r[-1] + [0]
+            l = map(lambda x, y: x + y, first_list, second_list)
+            r.append(l)
+
+        return r
+
 if __name__ == "__main__":
     s = Solution()
     print s.generate(4)
