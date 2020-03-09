fd1c9a036953cfb91c5452356b82f07fb75cf399
yu.xiang <yu.xiang@alibaba-inc.com>
2020-03-09 22:38:24 +0800

    x

---
+++ leetcode/string/interview 01.02. Check Permutation LCCI.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+
+'''
+Given two strings,write a method to decide if one is a permutation of the other.
+
+Example 1:
+
+Input: s1 = "abc", s2 = "bca"
+Output: true
+Example 2:
+
+Input: s1 = "abc", s2 = "bad"
+Output: false
+
+'''
+
+class Solution(object):
+    def CheckPermutation(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        return sorted(s1) == sorted(s2)
