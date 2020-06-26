d1b8c899086cbe22d05bed3d992dc8b9cfde0443
yu.xiang <yu.xiang@alibaba-inc.com>
2020-06-26 19:07:03 +0800

    x

--- metaclass_programming/metaclass.py
+++ metaclass_programming/metaclass.py
@@ -1,6 +1,9 @@
 # coding=utf-8
 
 '''
+
+python3
+
 动态语言和静态语言最大的不同，就是函数和类的定义，不是编译时定义的，而是运行时动态创建的。
 metaclass，直译为元类，先定义metaclass，就可以创建类，最后创建实例。
 '''
@@ -11,8 +14,8 @@ class Hello(object):
 
 h = Hello()
 h.hello()
-print type(Hello) # type()函数可以查看一个类型或变量的类型
-print type(h)
+print(type(Hello)) # type()函数可以查看一个类型或变量的类型
+print(type(h))
 
 
 # metaclass是类的模板，所以必须从`type`类型派生：
