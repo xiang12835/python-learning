5bcf75fe2114941b4d6a616348700eaef9e32542
yu.xiang <yu.xiang@alibaba-inc.com>
2020-06-26 19:21:49 +0800

    x

--- metaclass_programming/metaclass.py
+++ metaclass_programming/metaclass.py
@@ -5,7 +5,13 @@
 python3
 
 动态语言和静态语言最大的不同，就是函数和类的定义，不是编译时定义的，而是运行时动态创建的。
+
 metaclass，直译为元类，先定义metaclass，就可以创建类，最后创建实例。
+
+ORM就是一个典型的例子。虽然metaclass的编写会比较复杂，但ORM的使用者用起来却异常简单。
+
+metaclass是Python中非常具有魔术性的对象，它可以改变类创建时的行为。这种强大的功能使用起来务必小心。
+
 '''
 
 class Hello(object):
@@ -24,8 +30,10 @@ class ListMetaclass(type):
         attrs['add'] = lambda self, value: self.append(value)
         return type.__new__(cls, name, bases, attrs)
 
-class MyList(list, metaclass=ListMetaclass):
+class MyList(list, metaclass=ListMetaclass):  #  元类可以创建类
     pass
 
-l = MyList()
+l = MyList() # 通过创建的类来创建实例
 l.add(1)
+
+print(l)
