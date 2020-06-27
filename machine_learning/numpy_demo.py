194c26bd5afc630983d3e2ce5732f287efc4a6f5
yu.xiang <yu.xiang@alibaba-inc.com>
2020-06-27 23:26:52 +0800

    x

---
+++ machine_learning/numpy_demo.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+
+"""
+用于高性能科学计算和数据分析， 是常用的高级数据分析库的基础包
+"""
+
+import numpy as np
+
+arr1 = np.array([2,3,4])
+print(arr1)
+print(arr1.dtype)
+
+arr2 = np.array([2.1,3.1,4.1])
+print(arr2)
+print(arr2.dtype)
+
