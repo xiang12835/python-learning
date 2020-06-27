9051db83c2ee7d59b94b29b0f5c8bb5674c4a993
yu.xiang <yu.xiang@alibaba-inc.com>
2020-06-27 23:31:49 +0800

    x

--- machine_learning/numpy_demo.py
+++ machine_learning/numpy_demo.py
@@ -7,10 +7,17 @@
 import numpy as np
 
 arr1 = np.array([2,3,4])
-print(arr1)
-print(arr1.dtype)
+print arr1
+print arr1.dtype
 
 arr2 = np.array([2.1,3.1,4.1])
-print(arr2)
-print(arr2.dtype)
+print arr2
+print arr2.dtype
 
+print arr1 + arr2
+print arr1 * 10
+
+# 二维矩阵
+arr3 = np.array([[1,2,3],[2,3,4]])
+print arr3
+print arr3.dtype
