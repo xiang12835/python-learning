d56246b01bd7e627d5cd0eb6f642dacb83d9bfd3
yu.xiang <yu.xiang@alibaba-inc.com>
2020-07-07 00:18:54 +0800

    add112

---
+++ leetcode/recursion/112. Path Sum.py
@@ -0,0 +1,54 @@
+# coding=utf
+
+"""
+Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given the below binary tree and sum = 22,
+
+      5
+     / \
+    4   8
+   /   / \
+  11  13  4
+ /  \      \
+7    2      1
+return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def hasPathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+
+        递归
+
+假定从根节点到当前节点的值之和为 val，我们可以将这个大问题转化为一个小问题：是否存在从当前节点的子节点到叶子的路径，满足其路径和为 sum - val。
+
+若当前节点就是叶子节点，那么我们直接判断 sum 是否等于 val 即可（因为路径和已经确定，就是当前节点的值，我们只需要判断该路径和是否满足条件）。若当前节点不是叶子节点，我们只需要递归地询问它的子节点是否能满足条件即可。
+
+        时间复杂度：O(N)，其中 N 是树的节点数。对每个节点访问一次。
+        空间复杂度：O(logN)
+
+        """
+        if not root:
+            return False
+
+        if not root.left and not root.right:
+            return root.val == sum
+
+        return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
