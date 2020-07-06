ee6ec68362d01a7c1d46a11cbf55b3c15491cbb2
yu.xiang <yu.xiang@alibaba-inc.com>
2020-07-06 22:43:18 +0800

    x

--- leetcode/dp/62. Unique Paths.py
+++ leetcode/dp/62. Unique Paths.py
@@ -22,8 +22,8 @@ class Solution(object):
         T：O(m*n)
         S：O(m*n)
         """
-        dp = [[1]*m] + [[1] + [0]*(m-1) for _ in range(n-1)]
-        for i in range(1,n):
-            for j in range(1,m):
+        dp = [[1]*n] + [[1] + [0]*(n-1) for _ in range(m-1)]
+        for i in range(1,m):
+            for j in range(1,n):
                 dp[i][j] = dp[i-1][j] + dp[i][j-1]
         return dp[-1][-1]
