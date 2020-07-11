9ad17abd894fb912f7daa411cb1835a084adcc08
yu.xiang <yu.xiang@alibaba-inc.com>
2020-07-11 23:15:04 +0800

    x

--- leetcode/stack/20. Valid Parentheses.py
+++ leetcode/stack/20. Valid Parentheses.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 """
 Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 
@@ -40,7 +42,31 @@ class Solution2(object):
         return not stack
 
 
+class Solution3(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+
+        方法：栈
+
+        时间复杂度：O(n)O(n)，因为我们一次只遍历给定的字符串中的一个字符并在栈上进行 O(1)O(1) 的推入和弹出操作。
+空间复杂度：O(n)O(n)，当我们将所有的开括号都推到栈上时以及在最糟糕的情况下，我们最终要把所有括号推到栈上。例如 ((((((((((。
+        """
+        stack = []
+        d = {')': '(', ']': '[', '}': '{'}
+        for c in s:
+            if c not in d:  # 左刮号入栈
+                stack.append(c)
+            else:  # 右刮号，注意栈空
+                val = stack.pop() if stack else '#'
+                if d[c] != val:
+                    return False
+
+        return not stack
+
+
 if __name__ == "__main__":
-    s = Solution()
+    s = Solution3()
     print s.isValid("(){}")
     print s.isValid("()a{}")
