1b5ae768d6edd3e88999bbd5abbb79f4b569128d
yu.xiang <yu.xiang@alibaba-inc.com>
2020-07-27 15:22:57 +0800

    x

--- leetcode/string/344. Reverse String.py
+++ leetcode/string/344. Reverse String.py
@@ -15,13 +15,39 @@ class Solution(object):
         """
         return s[::-1]
 
-
 class Solution1(object):
     def reverseString(self, s):
         """
         :type s: str
         :rtype: str
+
+        return s[::-1]
+
+        """
+
+        def recursion(left, right):
+            """
+            T：O(N)，执行了 N/2 次的交换。
+            S：O(N)，递归过程中使用的堆栈空间。
+            """
+            if left >= right:
+                return
+            s[left], s[right] = s[right], s[left]
+            recursion(left + 1, right - 1)
+
+        recursion(0, len(s) - 1)
+
+
+
+class Solution2(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
         左右对撞
+
+        T：O(N)。执行了 N/2 次的交换。
+        S：O(1)，只使用了常数级空间。
         """
 
         ss = list(s)
