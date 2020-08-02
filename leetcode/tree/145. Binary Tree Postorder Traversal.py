3ea87e74b68ed2c776c28accd05ea1946ab0d98c
yu.xiang <yu.xiang@alibaba-inc.com>
2020-08-02 23:58:47 +0800

    add145

---
+++ leetcode/tree/145. Binary Tree Postorder Traversal.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+
+"""
+Given a binary tree, return the postorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [3,2,1]
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        def helper(root):
+            if not root:
+                return
+            helper(root.left)
+            helper(root.right)
+            res.append(root.val)
+        helper(root)
+
+        return res
