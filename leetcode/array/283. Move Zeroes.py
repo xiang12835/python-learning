cd9a3a1ae819946b4d081188679f955d26a9de55
shane <953762287@qq.com>
2021-01-30 11:52:08 +0800

    x

--- leetcode/array/283. Move Zeroes.py
+++ leetcode/array/283. Move Zeroes.py
@@ -73,3 +73,32 @@ class Solution2(object):
         while slow < n:
             nums[slow] = 0
             slow += 1
+
+class Solution3(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+
+        双指针
+
+        快速排序的思想:
+
+        快速排序首先要确定一个待分割的元素做中间点x，然后把所有小于等于x的元素放到x的左边，大于x的元素放到其右边。
+        这里我们可以用0当做这个中间点，把不等于0(注意题目没说不能有负数)的放到中间点的左边，等于0的放到其右边。
+        这的中间点就是0本身，所以实现起来比快速排序简单很多，我们使用两个指针i和j，只要nums[i]!=0，我们就交换nums[i]和nums[j]
+
+        T: O(n)
+        S: O(1)
+        """
+        if not nums:
+            return 0
+
+        # 两个指针i和j
+        n = len(nums)
+        j = 0
+        for i in range(n):
+            # 当前元素!=0，就把其交换到左边，等于0的交换到右边
+            if nums[i] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1
