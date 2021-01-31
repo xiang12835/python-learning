4905822c042d0728f1f5fc6b4d95fdedffb76119
shane <953762287@qq.com>
2021-02-01 07:56:42 +0800

    x

--- algorithm/dfs_template.py
+++ algorithm/dfs_template.py
@@ -30,3 +30,24 @@ def dfs_stack(self, tree):
         stack.push(nodes)
 
     # other processing work
+
+
+
+
+
+
+
+visited = set() # 和树中的DFS最大区别
+
+def dfs_graph(node, visited):
+    if node in visited:  # terminator
+        # already visited
+        return
+
+    visited.add(node)
+
+    # process current node here
+    …
+    for next_node in node.children():
+        if not next_node in visited:
+            dfs_graph(next_node, visited)
