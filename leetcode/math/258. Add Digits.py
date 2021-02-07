2c92b259085967f285daa1518369dcec5b04c994
shane <953762287@qq.com>
2021-02-07 22:52:03 +0800

    add_258

--- leetcode/math/258. Add Digits.py
+++ leetcode/math/258. Add Digits.py
@@ -12,3 +12,21 @@ class Solution:
             return num
         else:
             return num
+
+
+class Solution1(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+
+        将数字转成字符串，在求和
+
+        """
+        ss = str(num)
+        while (len(ss) > 1):
+            tmp = 0
+            for c in ss:
+                tmp += int(c)
+            ss = str(tmp)
+        return int(ss)
