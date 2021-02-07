ae5b46e7f4a223cb73f21d9d2a31fa4df754c060
shane <953762287@qq.com>
2021-02-07 23:01:03 +0800

    add_412

--- leetcode/math/412. Fizz Buzz.py
+++ leetcode/math/412. Fizz Buzz.py
@@ -2,6 +2,10 @@
 
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
+
+        """
+        取余
+        """
         r = []
         for i in range(1, n + 1):
             if i % 15 == 0:
