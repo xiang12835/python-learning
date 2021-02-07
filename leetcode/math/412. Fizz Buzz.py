af278b96c4692b0e4d4410d97a50f1c91bb2e7ec
shane <953762287@qq.com>
2021-02-07 23:00:07 +0800

    add_412

---
+++ leetcode/math/412. Fizz Buzz.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        r = []
+        for i in range(1, n + 1):
+            if i % 15 == 0:
+                r.append("FizzBuzz")
+            elif i % 3 == 0:
+                r.append("Fizz")
+            elif i % 5 == 0:
+                r.append("Buzz")
+            else:
+                r.append(str(i))
+
+        return r
