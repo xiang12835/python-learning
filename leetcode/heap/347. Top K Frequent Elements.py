39750430131eb6b5a9534780a85f77436143e488
shane <953762287@qq.com>
2021-02-07 18:42:07 +0800

    upd_347

--- leetcode/heap/347. Top K Frequent Elements.py
+++ leetcode/heap/347. Top K Frequent Elements.py
@@ -1,4 +1,4 @@
-
+# coding=utf-8
 
 """
  Given a non-empty array of integers, return the k most frequent elements.
@@ -20,6 +20,11 @@ class Solution(object):
         :type nums: List[int]
         :type k: int
         :rtype: List[int]
+
+        法一：字典 + 排序
+        T: O(nlogn)
+        S: O(n)
+
         """
         d = {}
         for n in nums:
@@ -28,11 +33,33 @@ class Solution(object):
             else:
                 d[n] = 1
 
+        # for n in nums:
+        #     d[n] = d.get(n, 0) + 1
+
 
         nums = list(set(nums))
 
         nums = sorted(nums, key=lambda num:d[num], reverse=True)
         return nums[:k]
+
+
+class Solution1:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        """
+
+        法二：去构建一个大顶堆，然后去删除k次，取k个最大值
+
+        T: O(nlogn)
+        S: O(n)
+        """
+
+        d = collections.Counter(nums)
+        hp, ans = [], []
+        for i in d:
+            heapq.heappush(hp, (-d[i], i))
+        for _ in range(k):
+            ans.append(heapq.heappop(hp)[1])  # 每删除一个都需要调整堆
+        return ans
 
 
 if __name__ == "__main__":
