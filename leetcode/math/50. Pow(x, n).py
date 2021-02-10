813acc53a47b34dd1bea899700d01b1c4754ad06
shane <953762287@qq.com>
2021-02-10 16:54:14 +0800

    upd_50

--- leetcode/math/50. Pow(x, n).py
+++ leetcode/math/50. Pow(x, n).py
@@ -43,3 +43,79 @@ class Solution(object):
             n >>= 1
 
         return r
+
+
+class Solution1:
+    def myPow(self, x: float, n: int) -> float:
+
+        """
+        暴力法
+
+        T：O(n)
+        S：O(1)
+
+        """
+        if n < 0:
+            n = -n
+            x = 1 / x
+
+        r = 1
+        for _ in range(n):
+            r *= x
+
+        return r
+
+
+class Solution2:
+    def myPow(self, x: float, n: int) -> float:
+        """
+        分治法
+
+        T：O(logn)，即为递归的层数。
+        S：O(logn)，即为递归的层数。这是由于递归的函数调用会使用栈空间。
+
+        """
+        def quickMul(N):
+            if N == 0:
+                return 1.0
+            y = quickMul(N // 2)
+            return y * y if N % 2 == 0 else y * y * x
+
+        return quickMul(n) if n >= 0 else 1.0 / quickMul(-n)
+
+
+class Solution3:
+    def myPow(self, x: float, n: int) -> float:
+        """
+        分治法
+
+        T：O(logn)，即为递归的层数。
+        S：O(logn)，即为递归的层数。这是由于递归的函数调用会使用栈空间。
+
+        """
+
+        if n < 0:
+            n = -n
+            x = 1 / x
+
+        return self._dc(x, n)
+
+    def _dc(self, x, n):
+        # terminator
+        if n == 0:
+            return 1.0
+
+        # process (split your big problems)
+        half = self._dc(x, n // 2)
+
+        # drill down (subproblems), merge (subresult)
+        if n % 2 == 0:
+            # even
+            return half * half
+        else:
+            # odd
+            return half * half * x
+
+        # reverse states
+
+
