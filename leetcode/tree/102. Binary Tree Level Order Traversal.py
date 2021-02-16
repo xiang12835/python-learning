e392e6ac8e7cf2894e69347484debdc2e81807cb
shane <953762287@qq.com>
2021-02-16 19:37:03 +0800

    upd_102

--- leetcode/tree/102. Binary Tree Level Order Traversal.py
+++ leetcode/tree/102. Binary Tree Level Order Traversal.py
@@ -64,34 +64,44 @@ class Solution(object):
 
 
 
-class Solution1(object):
-	def levelOrder(self, root):
-		"""
-		:type root: TreeNode
-		:rtype: List[List[int]]
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
+class Solution1:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        """
+        队列
 
-        时间复杂度：每个点进队出队各一次，故渐进时间复杂度为 O(n)。
-        空间复杂度：队列中元素的个数不超过 n 个，故渐进空间复杂度为 O(n)。
+        cur_level来记录当前层的数据
+        cur_size 当前这一层的节点个数
 
-		"""
-		if not root:
-			return []
-		res = []
-		queue = [root]
-		while queue:
-			# 获取当前队列的长度，这个长度相当于 当前这一层的节点个数
-			size = len(queue)
-			tmp = []
-			# 将队列中的元素都拿出来(也就是获取这一层的节点)，放到临时list中
-			# 如果节点的左/右子树不为空，也放入队列中
-			for _ in xrange(size):
-				r = queue.pop(0)
-				tmp.append(r.val)
-				if r.left:
-					queue.append(r.left)
-				if r.right:
-					queue.append(r.right)
-			# 将临时list加入最终返回结果中
-			res.append(tmp)
-		return res
+        T：每个点进队出队各一次，故渐进时间复杂度为 O(n)。
+        S：队列中元素的个数不超过 n 个，故渐进空间复杂度为 O(n)。
+        """
+        if not root:
+            return []
+
+        res = []
+        queue = [root]
+        while queue:
+            # 获取当前队列的长度，这个长度相当于 当前这一层的节点个数
+            cur_size = len(queue)
+            cur_level = []
+            # 将队列中的元素都拿出来(也就是获取这一层的节点)，放到临时list中
+            # 如果节点的左/右子树不为空，也放入队列中
+            for _ in range(cur_size):
+                node = queue.pop(0)  # 易错
+                cur_level.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            res.append(cur_level)
+
+        return res
