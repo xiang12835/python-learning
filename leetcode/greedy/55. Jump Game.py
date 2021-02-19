bceb8fdf5ed0f1f7f914ff34fb47f63adf23fa8f
shane <953762287@qq.com>
2021-02-19 18:35:34 +0800

    upd_55

--- leetcode/greedy/55. Jump Game.py
+++ leetcode/greedy/55. Jump Game.py
@@ -3,15 +3,14 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         """
+        思路：尽可能到达最远位置（贪心）。
+
         T : O(n)
         S : O(1)
         """
         n = len(nums)
-        dis = 0
+        max_dis = 0  # 初始化当前能到达最远的位置
         for i in range(n):
-            if i <= dis:
-                dis = max(dis, i + nums[i])
-                if dis >= n - 1:
-                    return True
-
-        return False
+            if i <= max_dis and i + nums[i] > max_dis:  # 如果当前位置能到达，并且当前位置+跳数>最远位置
+                max_dis = i + nums[i]  # 更新最远能到达位置
+        return max_dis >= i
