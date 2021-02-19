9adf4b6d55bc09ad46b7a545b0e09c034c2760b5
shane <953762287@qq.com>
2021-02-19 23:23:09 +0800

    upd_153

---
+++ leetcode/binsearch/153. Find Minimum in Rotated Sorted Array.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        """
+        二分查找
+
+        T: O(logN)
+        S: O(1)
+        """
+        if len(nums) == 1:
+            return nums[0]
+
+        l = 0
+        r = len(nums) - 1
+
+        if nums[r] > nums[0]:  # 易错，已经有序
+            return nums[0]
+        while l <= r:
+            mid = l + (r - l) // 2
+            if nums[mid] > nums[mid + 1]:
+                return nums[mid + 1]
+            if nums[mid - 1] > nums[mid]:
+                return nums[mid]
+            if nums[mid] > nums[0]:
+                l = mid + 1
+            else:
+                r = mid - 1
