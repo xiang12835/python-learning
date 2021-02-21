7f587c3a38c41c14ee7cb5189b3b2f8e438124ea
shane <953762287@qq.com>
2021-02-21 20:48:34 +0800

    upd_62

--- leetcode/dp/62. Unique Paths.py
+++ leetcode/dp/62. Unique Paths.py
@@ -27,3 +27,41 @@ class Solution(object):
             for j in range(1,n):
                 dp[i][j] = dp[i-1][j] + dp[i][j-1]
         return dp[-1][-1]
+
+
+class Solution1:
+    def uniquePaths(self, m, n):
+        """
+        dp
+
+        T: O(mn)
+        S: O(mn)
+        """
+        # 从 <start> 走到 (i, j) 的不同路径数
+        dp = [[0] * n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if i == 0 or j == 0:
+                    dp[i][j] = 1
+                else:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[-1][-1]
+
+
+class Solution2:
+    def uniquePaths(self, m, n):
+        """
+        dp
+
+        T: O(mn)
+        S: O(mn)
+        """
+        # 从 (i, j) 走到 <end> 的不同路径数
+        dp = [[0] * n for _ in range(m)]
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                if i == m - 1 or j == n - 1:
+                    dp[i][j] = 1
+                else:
+                    dp[i][j] = dp[i + 1][j] + dp[i][j + 1]
+        return dp[0][0]
