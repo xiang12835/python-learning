8ebf82135810acf57d82b7a71c603fe30fd2f1a5
shane <953762287@qq.com>
2021-03-02 15:07:22 +0800

    upd_23

--- leetcode/linked_list/23. Merge k Sorted Lists.py
+++ leetcode/linked_list/23. Merge k Sorted Lists.py
@@ -26,8 +26,70 @@ class ListNode(object):
 class Solution(object):
     def mergeKLists(self, lists):
         """
+        n 是所有链表中元素的总和，k 是链表个数。
+
+        法一：暴力，两两合并，O(KN)
+
+        自顶向下的编程思想
+
+        [[],[1]] 过不了
+        """
+        if not lists:
+            return
+
+        size = len(lists)
+        ans = lists[0]
+        for i in range(1, size):
+            self.mergeTwoLists(ans, lists[i])
+
+        return ans
+
+    def mergeTwoLists(self, l1, l2):
+        """
+        法一：迭代法 + 哨兵
+        T: O(m+n)
+        S: O(1)
+        """
+        dummy = tmp = ListNode(-1)
+        while l1 and l2:
+            if l1.val <= l2.val: # 为什么要有等于号？
+                tmp.next = l1
+                l1 = l1.next
+            else:
+                tmp.next = l2
+                l2 = l2.next
+            tmp = tmp.next
+
+        if l1:
+            tmp.next = l1
+        if l2:
+            tmp.next = l2
+
+        return dummy.next
+
+
+class Solution1(object):
+    def mergeKLists(self, lists):
+        """
         :type lists: List[ListNode]
         :rtype: ListNode
+
+        n 是所有链表中元素的总和，k 是链表个数。
+
+        法二：类似归并排序/快排：分治 O(NlogK)
+        """
+
+
+
+
+class Solution2(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+
+        法三：堆、优先队列 O(NlogK)
+
         哨兵；小顶堆
         """
         from heapq import heappush, heappop
