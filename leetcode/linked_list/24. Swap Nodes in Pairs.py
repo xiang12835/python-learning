efc1759bc9abef36b7c10f027bc68805e67d2552
shane <953762287@qq.com>
2021-03-04 20:52:04 +0800

    upd_24

--- leetcode/linked_list/24. Swap Nodes in Pairs.py
+++ leetcode/linked_list/24. Swap Nodes in Pairs.py
@@ -62,3 +62,37 @@ class Solution1:
             next_one.next = next_three
             cur = next_one
         return dummy.next
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution1:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        """
+        迭代法
+
+        T: O(N)
+        S: O(1)
+        """
+
+        if not head or not head.next:
+            return head # 易错
+
+        dummy = cur = ListNode(-1)
+        dummy.next = head # 易错，需要连接起来
+
+        while cur.next and cur.next.next:
+            one = cur.next
+            two = cur.next.next
+            tree = cur.next.next.next
+
+            cur.next = two
+            two.next = one
+            one.next = tree
+
+            cur = one
+
+        return dummy.next
