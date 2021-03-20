2c3a60bddff384a044e82011253d5d062d5b2bf3
shane <953762287@qq.com>
2021-03-20 22:25:36 +0800

    add_433

--- leetcode/bfs/433. Minimum Genetic Mutation.py
+++ leetcode/bfs/433. Minimum Genetic Mutation.py
@@ -19,3 +19,33 @@ class Solution:
                         queue.append((new, step+1))  # 入队，继续广度搜索
                         bank.remove(new)  # 避免重复遍历
         return -1  # 队列空了说明不可达
+
+
+class Solution1:
+    def minMutation(self, start: str, end: str, bank: List[str]) -> int:
+        """
+        双向BFS
+        """
+        if end not in bank:
+            return -1
+        start_set = {start}
+        end_set = {end}
+        bank = set(bank)
+        length = 0
+        change_map = {'A': 'TCG', 'T': 'ACG', 'C': 'ATG', 'G': 'ATC'}
+        while start_set:
+            length += 1
+            new_set = set()
+            for node in start_set:
+                for i, s in enumerate(node):
+                    for c in change_map[s]:
+                        new = node[:i] + c + node[i + 1:]
+                        if new in end_set:
+                            return length
+                        if new in bank:
+                            new_set.add(new)
+                            bank.remove(new)
+            start_set = new_set
+            if len(end_set) < len(start_set):
+                start_set, end_set = end_set, start_set
+        return -1
