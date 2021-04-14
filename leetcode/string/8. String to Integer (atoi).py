83d731aeb8f4badb6d375a49ac117828584e4f53
shane <953762287@qq.com>
2021-04-14 11:59:55 +0800

    upd_8

--- leetcode/string/8. String to Integer (atoi).py
+++ leetcode/string/8. String to Integer (atoi).py
@@ -157,6 +157,28 @@ class Solution2:
         return max(-2**31, min(sign * r,2**31-1))
 
 
+class Solution3:
+    def myAtoi(self, s: str) -> int:
+        s = s.strip()
+        if not s:
+            return 0
+
+        sign = -1 if s[0] == '-' else 1
+
+        if s[0] in ['+', '-']:
+            s = s[1:]
+
+        r = 0
+        for c in s:
+            if c.isdigit():
+                r = r * 10 + ord(c) - ord('0')
+            else:
+                break
+
+        r = sign * r
+
+        return max(-2 ** 31, min(r, 2 ** 31 - 1))
+
 if __name__ == "__main__":
     s = Solution()
     print s.myAtoi("  -0012a42")
