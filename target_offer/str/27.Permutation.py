dbd1abf3faa96d2af04bfea7c7e5abb31cba5bcf
shane <953762287@qq.com>
2021-04-30 14:21:21 +0800

    upd_27

--- target_offer/str/27.Permutation.py
+++ target_offer/str/27.Permutation.py
@@ -33,12 +33,22 @@ class Solution:
 
 class Solution1:
     def permutation(self, s):
+        """
+         回溯
+
+        path 变量是一个栈，对象类型变量在传参的过程中，复制的是变量的地址。这些地址被添加到 res 变量，但实际上指向的是同一块内存地址，因此我们会看到 6 个空的列表对象。解决的方法很简单，在 res.add(path); 这里做一次拷贝即可。
+
+        布尔数组 used，初始化的时候都为 false 表示这些数还没有被选择，当我们选定一个数的时候，就将这个数组的相应位置设置为 true ，这样在考虑下一个位置的时候，就能够以 O(1) 的时间复杂度判断这个数是否被选择过，这是一种「以空间换时间」的思想。
+
+        T: O(n×n!)
+        S: O(n×n!)
+        """
         size = len(s)
         if size == 0:
             return ""
 
         ss = list(s)
-        used = [False for _ in range(size)]
+        used = [False for _ in range(size)] # 布尔数组
         self.res = []
         self.backtrack(ss, size, 0, [], used)
         return self.res
