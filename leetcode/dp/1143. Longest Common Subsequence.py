f401d0a661e8595d87c166cdf516c836cfd0f5bf
shane <953762287@qq.com>
2021-06-04 14:29:09 +0800

    upd_1143

--- leetcode/dp/1143. Longest Common Subsequence.py
+++ leetcode/dp/1143. Longest Common Subsequence.py
@@ -2,8 +2,19 @@
 
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        """
-        dp
+        """dp
+
+        1. subproblem
+
+        2. dp array
+
+        dp[i][j] 表示 text1[0:i] 和 text2[0:j] 的最长公共子序列的长度。
+
+        3. dp equation
+
+        当 text1[i - 1] == text2[j - 1] 时，dp[i][j] = 1 + dp[i - 1][j - 1]
+
+        当 text1[i - 1] != text2[j - 1] 时，dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
 
         T: O(logmn)
         S: O(logmn)
