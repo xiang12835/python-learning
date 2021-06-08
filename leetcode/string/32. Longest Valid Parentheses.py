76ac35d0e8aa3301c0ac419264688f4ecdd41225
shane <953762287@qq.com>
2021-06-08 09:57:44 +0800

    upd_32

---
+++ leetcode/string/32. Longest Valid Parentheses.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+
+class Solution(object):
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s:
+            return 0
+
+        stack = [-1]
+        ans = 0
+        n = len(s)
+        for i in range(n):
+            # 入栈条件
+            if s[i] == '(':
+                stack.append(i)
+            else:
+                stack.pop()
+                if not stack:
+                    stack.append(i)
+                else:
+                    ans = max(ans, i - stack[-1])
+        return ans
