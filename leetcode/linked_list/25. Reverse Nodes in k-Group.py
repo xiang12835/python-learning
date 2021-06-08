d8ef2d08fa3933de5e8e3c27360ff84ed9f6dd2a
shane <953762287@qq.com>
2021-06-08 20:19:54 +0800

    upd_25

--- leetcode/linked_list/25. Reverse Nodes in k-Group.py
+++ leetcode/linked_list/25. Reverse Nodes in k-Group.py
@@ -39,13 +39,13 @@ class Solution:
 
         https://leetcode-cn.com/problems/reverse-nodes-in-k-group/solution/kge-yi-zu-fan-zhuan-lian-biao-by-powcai/
 
-    可以递归操作, 有两种情况：
+        可以递归操作, 有两种情况：
 
-    就是压根没有k个node，那么我们直接保持这个k-group不动返回head
-    如果有k个node的话，那么我们先找到第k个node之后的递归结果 node = nxt，然后反转前面k个node，让反转结果的结尾 tail.next = nxt
+        就是压根没有k个node，那么我们直接保持这个k-group不动返回head
+        如果有k个node的话，那么我们先找到第k个node之后的递归结果 node = nxt，然后反转前面k个node，让反转结果的结尾 tail.next = nxt
 
-    T: O(N)
-    S: O(1)
+        T: O(N)
+        S: O(1)
 
         """
         cur = head
