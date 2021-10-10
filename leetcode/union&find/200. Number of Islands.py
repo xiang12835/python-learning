0e62f630e4b63046519cf2dbaf8ab7ec8bc31a32
Shane Yoo <yu.xiang@alibaba-inc.com>
2021-10-10 17:57:55 +0800

    upd_200

--- leetcode/union&find/200. Number of Islands.py
+++ leetcode/union&find/200. Number of Islands.py
@@ -57,3 +57,36 @@ class Solution(object):
             return 1
 
         return 0
+
+
+class Solution1:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        """
+        dfs
+
+        T：O(MN)，其中 M 和 N 分别为行数和列数。
+        S：O(MN)，在最坏情况下，整个网格均为陆地，深度优先搜索的深度达到 M*N。
+        """
+        m = len(grid)
+        if not m:
+            return 0
+        n = len(grid[0])
+
+        def dfs(i, j):
+            if not 0 <= i < m or not 0 <= j < n or grid[i][j] != "1":
+                return
+            grid[i][j] = '0'  # 将岛屿都夷为平地
+            dfs(i - 1, j)
+            dfs(i + 1, j)
+            dfs(i, j - 1)
+            dfs(i, j + 1)
+
+        # 两层循环扫描，当发现是陆地时，将其周围相邻的岛屿都夷为平地
+        count = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1":
+                    count += 1
+                    dfs(i, j)
+
+        return count
