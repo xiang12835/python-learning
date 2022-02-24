b0138aa8b64231c11e4309117a59cd087a71d84a
Shane Yoo <yu.xiang@alibaba-inc.com>
2022-02-24 22:33:32 +0800

    add_130

---
+++ leetcode/dfs/130. Surrounded Regions.py
@@ -0,0 +1,50 @@
+# coding=utf-8
+
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """ dfs
+        Do not return anything, modify board in-place instead.
+
+        对于每一个边界上的 O，我们以它为起点，标记所有与它直接或间接相连的字母 O；
+
+        最后我们遍历这个矩阵，对于每一个字母：
+        如果该字母被标记过，则该字母为没有被字母 X 包围的字母 O，我们将其还原为字母 O；
+        如果该字母没有被标记过，则该字母为被字母 X 包围的字母 O，我们将其修改为字母 X。
+
+        T: O(mn)
+        S: O(mn)
+        """
+
+        if not board:
+            return
+
+        m = len(board)
+        n = len(board[0])
+
+        # 对于每一个边界上的 O，我们以它为起点，标记所有与它直接或间接相连的字母 O
+        def dfs(x, y):
+            if not 0 <= x < m or not 0 <= y < n or board[x][y] != "O":
+                return
+            board[x][y] = "#"
+            dfs(x-1, y)
+            dfs(x+1, y)
+            dfs(x, y-1)
+            dfs(x, y+1)
+
+        # 边界 - 列
+        for i in range(m):
+            dfs(i, 0)
+            dfs(i, n-1)
+
+        # 边界 - 行
+        for j in range(n):
+            dfs(0, j)
+            dfs(m-1, j)
+
+        # 修改
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == "#":
+                    board[i][j] = "O"
+                elif board[i][j] == "O":
+                    board[i][j] = "X"
