45af10a628556f5e3f384d5d0d4e3d2513d49fed
Shane Yoo <yu.xiang@alibaba-inc.com>
2022-02-26 17:09:13 +0800

    x

--- leetcode/backtrack/37. Sudoku Solver.py
+++ leetcode/backtrack/37. Sudoku Solver.py
@@ -6,36 +6,37 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: None Do not return anything, modify board in-place instead.
         """
-        row = [set(range(1, 10)) for _ in range(9)]  # 行剩余可用数字
-        col = [set(range(1, 10)) for _ in range(9)]  # 列剩余可用数字
-        block = [set(range(1, 10)) for _ in range(9)]  # 块剩余可用数字
-
-        empty = []  # 收集需填数位置
-        for i in range(9):
-            for j in range(9):
-                if board[i][j] != '.':  # 更新可用数字
-                    val = int(board[i][j])
-                    row[i].remove(val)
-                    col[j].remove(val)
-                    block[(i // 3) * 3 + j // 3].remove(val)
-                else:
-                    empty.append((i, j))
-
-        def backtrack(iter=0):
-            if iter == len(empty):  # 处理完empty代表找到了答案
-                return True
-            i, j = empty[iter]
-            b = (i // 3) * 3 + j // 3
-            for val in row[i] & col[j] & block[b]:
-                row[i].remove(val)
-                col[j].remove(val)
-                block[b].remove(val)
-                board[i][j] = str(val)
-                if backtrack(iter + 1):
-                    return True
-                row[i].add(val)  # 回溯
-                col[j].add(val)
-                block[b].add(val)
+        if not board:
             return False
 
-        backtrack()
+        m = len(board)
+        n = len(board[0])
+
+        def backtrack(board):
+            for i in range(m):  #遍历行
+                for j in range(n):  #遍历列
+                    if board[i][j] != ".": continue
+                    for k in range(1,10):  #(i, j) 这个位置放k是否合适
+                        if isValid(i,j,k,board):
+                            board[i][j] = str(k)  #放置k
+                            if backtrack(board): return True  #如果找到合适一组立刻返回
+                            board[i][j] = "."  #回溯，撤销k
+                    return False  #9个数都试完了，都不行，那么就返回false
+            return True  #遍历完没有返回false，说明找到了合适棋盘位置了
+
+        def isValid(row,col,val,board):
+            for i in range(9):  #判断行里是否重复
+                if board[row][i] == str(val):
+                    return False
+            for j in range(9):  #判断列里是否重复
+                if board[j][col] == str(val):
+                    return False
+            brow = (row // 3) * 3
+            bcol = (col // 3) * 3
+            for i in range(brow,brow + 3):  #判断9方格里是否重复
+                for j in range(bcol,bcol + 3):
+                    if board[i][j] == str(val):
+                        return False
+            return True
+
+        backtrack(board)
