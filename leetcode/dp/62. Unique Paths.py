15fe61a9e815be76551aec235e22875fcb8b00dc
Shane Yoo <yu.xiang@alibaba-inc.com>
2022-02-26 20:16:14 +0800

    add_62

--- leetcode/dp/62. Unique Paths.py
+++ leetcode/dp/62. Unique Paths.py
@@ -30,6 +30,34 @@ class Solution(object):
 
 
 class Solution1:
+    def uniquePaths(self, m: int, n: int) -> int:
+        """
+        a. subproblems
+        b. dp array : dp[i][j] 是到达 i, j 最多路径
+        c. dp equation : dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+        注意，对于第一行 dp[0][j]，或者第一列 dp[i][0]，由于都是在边界，所以只能为 1
+
+        T：O(m*n)
+        S：O(m*n)
+        """
+        dp = [[0]*n for _ in range(m)]
+
+        # 列
+        for j in range(n):
+            dp[0][j] = 1
+
+        # 行
+        for i in range(m):
+            dp[i][0] = 1
+
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+        return dp[-1][-1]
+
+class Solution2:
     def uniquePaths(self, m, n):
         """
         dp
@@ -48,7 +76,7 @@ class Solution1:
         return dp[-1][-1]
 
 
-class Solution2:
+class Solution3:
     def uniquePaths(self, m, n):
         """
         dp
