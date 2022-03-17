8aeda613db30a02c0191731c3096bed4fdd24b31
Shane Yoo <yu.xiang@alibaba-inc.com>
2022-03-17 15:04:49 +0800

    upd_389

--- leetcode/string/389. Find the Difference.py
+++ leetcode/string/389. Find the Difference.py
@@ -4,4 +4,13 @@ from collections import Counter
 
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
+        """
+        tc = Counter({'a': 1, 'b': 1, 'c': 1, 'd': 1, 'e': 1})
+
+        sc = Counter({'a': 1, 'b': 1, 'c': 1, 'd': 1})
+
+        tc - sc = Counter({'e': 1})
+
+        list(tc - sc) = ['e']
+        """
         return list(Counter(t) - Counter(s))[0]
