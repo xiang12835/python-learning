e178ddcfce30f277bc696759b1b6a5b597d75b8e
Shane Yoo <yu.xiang@alibaba-inc.com>
2022-03-19 13:07:19 +0800

    add_113

--- leetcode/tree/113. Path Sum II.py
+++ leetcode/tree/113. Path Sum II.py
@@ -20,4 +20,32 @@ return
 
 """
 
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        """
+        dfs
+
+        T: O(N^2)
+        S: O(N)
+        """
+
+        def dfs(node, sum, path):
+            # 空节点，不处理
+            if not node:
+                return
+
+                # 叶子节点
+            if not node.left and not node.right:
+                if node.val == sum:
+                    res.append(path+[node.val])
+
+            # 左子树
+            dfs(node.left, sum-node.val, path+[node.val])
+
+            # 右子树
+            dfs(node.right, sum-node.val, path+[node.val])
+
+        res = []
+        dfs(root, targetSum, [])
+        return res
 
