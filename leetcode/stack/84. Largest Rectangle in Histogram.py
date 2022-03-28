512ca1287e7d9a5574937ac2dc0cc8acceaa9a56
Shane Yoo <yu.xiang@alibaba-inc.com>
2022-03-28 22:44:53 +0800

    upd_84

--- leetcode/stack/84. Largest Rectangle in Histogram.py
+++ leetcode/stack/84. Largest Rectangle in Histogram.py
@@ -1,9 +1,9 @@
 from typing import List
 
 
-class Solution:
-    def largestRectangleArea(self, heights: List[int]) -> int:
-        """
+class Solution(object):
+    def largestRectangleArea(self, heights):
+        """ 单调栈
         https://leetcode-cn.com/problems/largest-rectangle-in-histogram/solution/bao-li-jie-fa-zhan-by-liweiwei1419/
 
         动画演示
@@ -11,22 +11,24 @@ class Solution:
 
         时间复杂度：O(N)，输入数组里的每一个元素入栈一次，出栈一次。
         空间复杂度：O(N)，栈的空间最多为 N。
-
         """
-        size = len(heights)
-        res = 0
+        n = len(heights)
+
         heights = [0] + heights + [0]
         # 先放入哨兵结点，在循环中就不用做非空判断
-        stack = [0]
-        size += 2
+        stack = [0] # 存放下标
+        n += 2
 
-        for i in range(1, size):
+        res = 0
+        for i in range(1, n):
             while heights[i] < heights[stack[-1]]:
                 cur_height = heights[stack.pop()]
                 cur_width = i - stack[-1] - 1
                 res = max(res, cur_height * cur_width)
             stack.append(i)
+
         return res
+
 
 
 if __name__ == '__main__':
