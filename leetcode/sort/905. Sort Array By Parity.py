14a0c0fef57806a6ac1b2274d88d02bf773dfe11
Shane Yoo <yu.xiang@alibaba-inc.com>
2022-04-10 10:11:27 +0800

    add_905

--- leetcode/sort/905. Sort Array By Parity.py
+++ leetcode/sort/905. Sort Array By Parity.py
@@ -40,3 +40,35 @@ class Solution3:
                 right -= 1
 
         return nums
+
+class Solution4:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        """
+        法三：快排思想 + 原地
+
+        如果是 (0, 1)，那么万事大吉 i++ 并且 j--。
+        如果是 (1, 0)，那么交换两个元素，然后继续。
+        如果是 (0, 0)，那么说明 i 位置是正确的，只能 i++。
+        如果是 (1, 1)，那么说明 j 位置是正确的，只能 j--。
+
+        T: O(N)
+        S: O(1)
+        """
+
+        l = 0
+        r = len(nums) - 1
+        while l < r:
+            if nums[l] % 2 == 1 and nums[r] % 2 == 0: # (1,0)
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+                r -= 1
+            elif nums[l] % 2 == 0 and nums[r] % 2 == 1:
+                l += 1
+                r -= 1
+            elif nums[l] % 2 == 0 and nums[r] % 2 == 0:
+                l += 1
+            elif nums[l] % 2 == 1 and nums[r] % 2 == 1:
+                r -= 1
+
+        return nums
+
