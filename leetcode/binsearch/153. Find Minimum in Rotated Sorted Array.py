6f25f70bd33652612dbb1e00d0b6783a741565bb
Shane Yoo <yu.xiang@alibaba-inc.com>
2022-06-19 18:22:17 +0800

    x

--- leetcode/binsearch/153. Find Minimum in Rotated Sorted Array.py
+++ leetcode/binsearch/153. Find Minimum in Rotated Sorted Array.py
@@ -28,6 +28,27 @@ class Solution:
                 r = mid - 1
 
 
+class Solution1:
+    def findMin(self, nums: List[int]) -> int:
+        """
+        二分查找
+
+        T: O(logN)
+        S: O(1)
+
+        无重复元素
+        """
+        n = len(nums)
+        l = 0
+        r = n - 1
+        while l < r:                  # 循环不变式，如果left == right，则循环结束
+            mid = l + (r-l)//2        # 区间的中间点
+            if nums[mid] < nums[r]:   # 明确中值 < 右值，最小值在左半边，收缩右边界
+                r = mid               # 因为中值 < 右值，中值也可能是最小值，右边界只能取到mid处
+            elif nums[mid] > nums[r]: # 中值 > 右值，最小值在右半边，收缩左边界
+                l = mid + 1           # 因为中值 > 右值，中值肯定不是最小值，左边界可以跨过mid
+        return nums[l]                # 循环结束，left == right，最小值输出nums[left]或nums[right]均可
+
 if __name__ == "__main__":
     nums = [2,4,5,6,7,0,1]
 
--- leetcode/binsearch/154. Find Minimum in Rotated Sorted Array II.py
+++ leetcode/binsearch/154. Find Minimum in Rotated Sorted Array II.py
@@ -2,24 +2,24 @@
 
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        """ 二分法 + 暴力
-        https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array-ii/solution/xun-zhao-xuan-zhuan-pai-xu-shu-zu-zhong-de-zui--16/
-
-        可重复
+        """
+        二分查找
 
         T: O(logN)
         S: O(1)
+
+        有重复元素
         """
         n = len(nums)
         l = 0
         r = n - 1
-        while l <= r:
-            m = l + (r - l) // 2
-            if nums[m] > nums[r]:
-                l = m + 1
-            elif nums[m] < nums[r]:
-                r = m
+        while l < r:                  # 循环不变式，如果left == right，则循环结束
+            mid = l + (r - l) // 2    # 区间的中间点
+            if nums[mid] < nums[r]:   # 明确中值 < 右值，最小值在左半边，收缩右边界
+                r = mid               # 因为中值 < 右值，中值也可能是最小值，右边界只能取到mid处
+            elif nums[mid] > nums[r]: # 中值 > 右值，最小值在右半边，收缩左边界
+                l = mid + 1           # 因为中值 > 右值，中值肯定不是最小值，左边界可以跨过mid
             else:
-                r -= 1 # 暴力缩小范围
+                r = r - 1             # 不能确定 nums[mid] 究竟在最小值的左侧还是右侧
 
-        return nums[l]
+        return nums[l]                # 循环结束，left == right，最小值输出nums[left]或nums[right]均可
